236dcf65ffd1e9109d40a110a5c9f6122f6d321a
Giulia Campana <giuliaecampana@gmail.com>
2019-03-22 15:35:10 -0700

    Put all controls in index

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,9 +1,17 @@
 import BackNavButton from './buttons/BackNavButton';
 import PrimaryButton from './buttons/PrimaryButton';
 import StyledButton from './buttons/StyledButton';
+import SecondaryButton from './buttons/SecondaryButton';
+import StyledInput from './StyledInput';
+import StyledSelect from './StyledSelect';
+import ToolBar from './ToolBar';
 
 export {
   BackNavButton,
   PrimaryButton,
+  SecondaryButton,
   StyledButton,
+  StyledInput,
+  StyledSelect,
+  ToolBar,
 };
