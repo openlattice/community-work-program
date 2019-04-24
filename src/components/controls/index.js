46363ecadf6322812b1bc63e8381584fafc5730b
Giulia Campana <giuliaecampana@gmail.com>
2019-04-24 13:35:50 -0700

    Add TertiaryButton and index file to export buttons from

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,9 +1,13 @@
 import BackNavButton from './buttons/BackNavButton';
+import NavButton from './buttons/NavButton';
 import PrimaryButton from './buttons/PrimaryButton';
 import StyledButton from './buttons/StyledButton';
+import TertiaryButton from './buttons/TertiaryButton';
 
 export {
   BackNavButton,
+  NavButton,
   PrimaryButton,
   StyledButton,
+  TertiaryButton,
 };
