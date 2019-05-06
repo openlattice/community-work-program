401ae8a47f78c3fb41f924a6ebd05ff633b1ef08
Giulia Campana <giuliaecampana@gmail.com>
2019-05-06 14:51:34 -0700

    Add AppTypes to FQNs file along with file that converts those FQNs to strings

--- src/core/edm/constants/FullyQualifiedNames.js
+++ src/core/edm/constants/FullyQualifiedNames.js
@@ -6,15 +6,29 @@ import { Models } from 'lattice';
 
 const { FullyQualifiedName } = Models;
 
-const ASSOCIATION_ENTITY_TYPE_FQNS = {};
-const ENTITY_TYPE_FQNS = {};
-
-const PROPERTY_TYPE_FQNS = {
-  OL_ID_FQN: new FullyQualifiedName('ol.id'),
-};
-
-export {
-  ASSOCIATION_ENTITY_TYPE_FQNS,
-  ENTITY_TYPE_FQNS,
-  PROPERTY_TYPE_FQNS,
+export const APP_TYPE_FQNS = {
+  APP_SETTINGS: new FullyQualifiedName('app.settings'),
+  APPEARS_IN: new FullyQualifiedName('app.appearsin'),
+  APPOINTMENT: new FullyQualifiedName('app.appointment'),
+  ASSESSED_BY: new FullyQualifiedName('app.assessedby'),
+  CHARGED_WITH: new FullyQualifiedName('app.chargedwith'),
+  CONTACT_INFO_GIVEN: new FullyQualifiedName('app.contactinfogiven'),
+  CONTACT_INFORMATION: new FullyQualifiedName('app.contactinformation'),
+  COURT_PRETRIAL_CASES: new FullyQualifiedName('app.courtpretrialcases'),
+  EDITED_BY: new FullyQualifiedName('app.editedby'),
+  JUDGES: new FullyQualifiedName('app.judges'),
+  LOCATION: new FullyQualifiedName('app.location'),
+  MANUAL_PRETRIAL_CASES: new FullyQualifiedName('app.manualpretrialcases'),
+  MANUAL_SENTENCES: new FullyQualifiedName('app.manualsentences'),
+  ORGANIZATION: new FullyQualifiedName('app.organization'),
+  PEOPLE: new FullyQualifiedName('app.people'),
+  PROGRAM_OUTCOME: new FullyQualifiedName('app.programoutcome'),
+  REGISTERED_FOR: new FullyQualifiedName('app.registeredfor'),
+  REMINDER_SENT: new FullyQualifiedName('app.remindersent'),
+  SENTENCE_TERM: new FullyQualifiedName('app.sentenceterm'),
+  SENTENCED_WITH: new FullyQualifiedName('app.sentencedwith'),
+  SENTENCES: new FullyQualifiedName('app.sentences'),
+  STAFF: new FullyQualifiedName('app.staff'),
+  WORKS_AT: new FullyQualifiedName('app.worksat'),
+  WORKSITE: new FullyQualifiedName('app.worksite'),
 };
