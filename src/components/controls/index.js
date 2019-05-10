56bd9f16d18b3ba0fb0bf4ff1d0475fd5caec343
Giulia Campana <giuliaecampana@gmail.com>
2019-05-10 15:37:50 -0700

    Update controls index

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,9 +1,13 @@
 import BackNavButton from './buttons/BackNavButton';
-import NavButton from './buttons/NavButton';
+import EditButton from './buttons/EditButton';
+import { NavButton } from './buttons/NavButton';
 import PrimaryButton from './buttons/PrimaryButton';
 import StyledButton from './buttons/StyledButton';
 import TertiaryButton from './buttons/TertiaryButton';
 import TertiaryButtonLink from './buttons/TertiaryButtonLink';
+
+import DateWidget from './datetime/DateWidget';
+import TimeWidget from './datetime/TimeWidget';
 
 import StyledFunctionSelect from './dropdowns/StyledFunctionSelect';
 import StyledSelect from './dropdowns/StyledSelect';
@@ -14,6 +18,8 @@ import ToolBar from './ToolBar';
 
 export {
   BackNavButton,
+  DateWidget,
+  EditButton,
   NavButton,
   PrimaryButton,
   StyledButton,
@@ -22,5 +28,6 @@ export {
   StyledSelect,
   TertiaryButton,
   TertiaryButtonLink,
+  TimeWidget,
   ToolBar,
 };
