d9caa5431c2d4b2f6f4918aedf14a19a44f9c3c4
Giulia Campana <giuliaecampana@gmail.com>
2019-05-10 15:39:21 -0700

    Create GeneralConsts file and add WorkSchedule menus to it

---
+++ src/utils/constants/GeneralConsts.js
@@ -0,0 +1,13 @@
+/* Work Schedules */
+
+export const participantScheduleMenu = ['All', 'Upcoming', 'Past'];
+export const scheduleMenu = [
+  'All',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
