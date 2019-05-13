d8415a5acdce94da0367c87b2a72da381cf3ab0a
Giulia Campana <giuliaecampana@gmail.com>
2019-05-13 14:41:20 -0700

    Add tentative entity sets to add as comments to AppType FQNs

--- src/core/edm/constants/FullyQualifiedNames.js
+++ src/core/edm/constants/FullyQualifiedNames.js
@@ -9,28 +9,42 @@ const { FullyQualifiedName } = Models;
 export const APP_TYPE_FQNS = {
   APP_SETTINGS: new FullyQualifiedName('app.settings'),
   APPEARS_IN: new FullyQualifiedName('app.appearsin'),
-  APPOINTMENT: new FullyQualifiedName('app.appointment'),
+  APPOINTMENT: new FullyQualifiedName('app.appointment'), // delete?
   ASSESSED_BY: new FullyQualifiedName('app.assessedby'),
+  // BASED_ON: new FullyQualifiedName('app.basedon'),
   CHARGED_WITH: new FullyQualifiedName('app.chargedwith'),
+  // CHECK_IN: new FullyQualifiedName('app.checkin'),
   CONTACT_INFO_GIVEN: new FullyQualifiedName('app.contactinfogiven'),
   CONTACT_INFORMATION: new FullyQualifiedName('app.contactinformation'),
+  // CONTACTED_VIA: new FullyQualifiedName('app.contactedvia'),
   COURT_PRETRIAL_CASES: new FullyQualifiedName('app.courtpretrialcases'),
+  // DIVERSION_PLAN: new FullyQualifiedName('app.diversionplan'),
   EDITED_BY: new FullyQualifiedName('app.editedby'),
+  // ENROLLMENT_STATUS: new FullyQualifiedName('app.enrollmentstatus'),
+  // ELIGIBLE_FOR: new FullyQualifiedName('app.eligiblefor'),
+  // HAS: new FullyQualifiedName('has'),
+  // INFRACTION: new FullyQualifiedName('app.infraction'),
   JUDGES: new FullyQualifiedName('app.judges'),
+  // LOCATED_AT: new FullyQualifiedName('app.locatedat'),
   LOCATION: new FullyQualifiedName('app.location'),
-  MANUAL_PRETRIAL_CASES: new FullyQualifiedName('app.manualpretrialcases'),
-  MANUAL_SENTENCES: new FullyQualifiedName('app.manualsentences'),
+  MANUAL_PRETRIAL_CASES: new FullyQualifiedName('app.manualpretrialcases'), // delete
+  MANUAL_SENTENCES: new FullyQualifiedName('app.manualsentences'), // delete
+  // OF_LENGTH: new FullyQualifiedName('app.oflength'),
   ORGANIZATION: new FullyQualifiedName('app.organization'),
   PEOPLE: new FullyQualifiedName('app.people'),
+  // PROGRAM: new FullyQualifiedName('app.program'),
   PROGRAM_OUTCOME: new FullyQualifiedName('app.programoutcome'),
   REGISTERED_FOR: new FullyQualifiedName('app.registeredfor'),
   REMINDER_SENT: new FullyQualifiedName('app.remindersent'),
+  // RESULTS_IN: new FullyQualifiedName('app.resultsin'),
+  // SCHEDULE: new FullyQualifiedName('app.schedule'),
   SENTENCE_TERM: new FullyQualifiedName('app.sentenceterm'),
   SENTENCED_WITH: new FullyQualifiedName('app.sentencedwith'),
   SENTENCES: new FullyQualifiedName('app.sentences'),
-  STAFF: new FullyQualifiedName('app.staff'),
+  STAFF: new FullyQualifiedName('app.staff'), // should be type generalcontactedvia.person
   WORKS_AT: new FullyQualifiedName('app.worksat'),
   WORKSITE: new FullyQualifiedName('app.worksite'),
+  // WORKSITE_PLAN: new FullyQualifiedName('app.worksiteplan'),
 };
 
 export const PEOPLE_FQNS = {
