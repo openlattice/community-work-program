9670d0ad8a4396bc77ebabd0d35fe3470590e048
Giulia Campana <giuliaecampana@gmail.com>
2019-05-14 15:48:51 -0700

    Add Routes for edit modals to ParticipantsContainer

--- src/containers/participants/ParticipantsContainer.js
+++ src/containers/participants/ParticipantsContainer.js
@@ -2,20 +2,26 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Map } from 'immutable';
+import type { RouterHistory } from 'react-router';
 
 import ParticipantsSearchContainer from './ParticipantsSearchContainer';
 import ParticipantProfile from '../participant/ParticipantProfile';
 import NewWarningViolationContainer from '../violations/NewWarningViolationContainer';
 import NewAppointmentContainer from '../schedule/NewAppointmentContainer';
-
+import EditModal from '../edit/EditModal';
+import EditGeneralInfo from '../../components/participant/EditGeneralInfo';
+import EditCaseNumber from '../../components/participant/EditCaseNumber';
 import * as Routes from '../../core/router/Routes';
 
+
 type State = {
+  isEditModalVisible :boolean;
   selectedPerson :Map;
   selectedContactInfo :Map;
 };
 
 type Props = {
+  history :RouterHistory;
 };
 
 class ParticipantsContainer extends Component<Props, State> {
@@ -23,9 +29,21 @@ class ParticipantsContainer extends Component<Props, State> {
     super(props);
 
     this.state = {
+      isEditModalVisible: false,
       selectedPerson: Map(),
       selectedContactInfo: Map(),
     };
+  }
+
+  renderModalVisible = () => {
+    this.setState({ isEditModalVisible: true });
+  }
+
+  renderModalNotVisible = () => {
+    const { history } = this.props;
+    const { selectedPerson } = this.state;
+    this.setState({ isEditModalVisible: false });
+    history.push(Routes.PARTICIPANT_PROFILE.replace(':subjectId', selectedPerson.get('personId')));
   }
 
   selectPerson = (selectedPerson :Map, selectedContactInfo :Map) => {
@@ -33,7 +51,7 @@ class ParticipantsContainer extends Component<Props, State> {
   }
 
   render() {
-    const { selectedPerson, selectedContactInfo } = this.state;
+    const { isEditModalVisible, selectedPerson, selectedContactInfo } = this.state;
     return (
       <Switch>
         <Route
@@ -51,11 +69,38 @@ class ParticipantsContainer extends Component<Props, State> {
                   {...props} />
             )} />
         <Route
+            path={Routes.PARTICIPANT_GENERAL_INFO_EDIT}
+            render={props => (
+              <EditModal
+                  body={EditGeneralInfo}
+                  formData={Map()}
+                  isVisible={isEditModalVisible}
+                  onClose={this.renderModalNotVisible}
+                  person={selectedPerson}
+                  textPrimary="Submit"
+                  textTitle="Edit General Info"
+                  {...props} />
+            )} />
+        <Route
+            path={Routes.PARTICIPANT_CASE_EDIT}
+            render={props => (
+              <EditModal
+                  body={EditCaseNumber}
+                  formData={Map()}
+                  isVisible={isEditModalVisible}
+                  onClose={this.renderModalNotVisible}
+                  person={selectedPerson}
+                  textPrimary="Submit"
+                  textTitle="Edit Case Number"
+                  {...props} />
+            )} />
+        <Route
             path={Routes.PARTICIPANT_PROFILE}
             render={props => (
               <ParticipantProfile
                   contactInfo={selectedContactInfo}
                   person={selectedPerson}
+                  renderModalVisible={this.renderModalVisible}
                   {...props} />
             )} />
         <Route
--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -11,3 +11,5 @@ export const WORKSITES :string = '/worksites';
 export const PARTICIPANT_PROFILE :string = `${PARTICIPANTS}/:subjectId`;
 export const NEW_APPOINTMENT :string = `${PARTICIPANT_PROFILE}/newappointment`;
 export const WARNINGS_VIOLATIONS_FORM :string = `${PARTICIPANT_PROFILE}/reportwarningorviolation`;
+export const PARTICIPANT_GENERAL_INFO_EDIT :string = `${PARTICIPANT_PROFILE}/editgeneralinfo`;
+export const PARTICIPANT_CASE_EDIT :string = `${PARTICIPANT_PROFILE}/editcasenumber`;
