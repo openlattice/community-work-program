fd19b50e46a770427e62f552c5920cf4f23ab19e
Giulia Campana <giuliaecampana@gmail.com>
2019-05-28 15:35:07 -0700

    Add prod CWP Demo Org ID to consts file

--- src/core/edm/constants/DataModelConsts.js
+++ src/core/edm/constants/DataModelConsts.js
@@ -5,5 +5,6 @@
 export const APP_NAME = 'communityworkprogram';
 
 export const ORG_IDS = {
-  DEMO_ORG: '1d5aa1f4-4d22-46a5-97cd-dcc6820e7ff8',
+  CWP_DEMO_ORG: '030c9cc56d7e4388be54e148f27b3f01',
+  OL_DEMO_ORG: '1d5aa1f4-4d22-46a5-97cd-dcc6820e7ff8',
 };
