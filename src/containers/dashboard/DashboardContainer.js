b0ced3d0fe7cd6e786d1c6b8da7815fdf57f6610
Giulia Campana <giuliaecampana@gmail.com>
2019-05-30 14:45:03 -0700

    Change DashboardContainer to a stateful component

--- src/containers/dashboard/DashboardContainer.js
+++ src/containers/dashboard/DashboardContainer.js
@@ -1,18 +1,20 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { List, Map } from 'immutable';
-import { Switch } from 'react-router-dom';
+import { RequestStates } from 'redux-reqseq';
+import type { RequestState } from 'redux-reqseq';
 
 import NewParticipantsTable from '../../components/table/NewParticipantsTable';
 import PendingReviewParticipantsTable from '../../components/table/PendingReviewParticipantsTable';
 import ViolationsParticipantsTable from '../../components/table/ViolationsParticipantsTable';
+import LogoLoader from '../../components/LogoLoader';
 
 import {
   APP_CONTENT_PADDING,
   DASHBOARD_WIDTH,
 } from '../../core/style/Sizes';
-import { people } from './FakeData';
+import { GET_PARTICIPANTS } from '../participants/ParticipantsActions';
 
 /* styled components */
 const DashboardWrapper = styled.div`
@@ -39,41 +41,104 @@ const RightWrapper = styled.div`
   margin: 0 0 30px 30px;
 `;
 
-/* constants */
-const newParticipants = people.filter((person :Map) => person.get('status') === 'Awaiting enrollment');
-const pendingCompletionReview = people.filter((person :Map) => (
-  person.get('status').includes('Active') && person.get('requiredHours') === person.get('hoursServed')
-));
-const violationsWatch = people.filter((person :Map) => (
-  person.get('numberOfViolations') > 0 && person.get('status').includes('Active')
-));
+type Props = {
+  enrollmentByParticipant :Map;
+  getParticipantsRequestState :RequestState;
+  infractionsByParticipant :Map;
+  participants :List;
+  sentencesByParticipant :Map;
+  resetRequestState :(actionType :string) => void;
+};
+
+type State = {
+  newParticipants :List;
+  pendingCompletionReview :List;
+  violationsWatch :List;
+};
 
 /* react component */
-const Dashboard = () => (
-  <DashboardWrapper>
-    <DashboardBody>
-      <NewParticipantsTable
-          handleSelect={() => {}}
-          people={newParticipants}
-          selectedPersonId=""
-          small
-          totalParticipants={newParticipants.count()} />
-      <RightWrapper>
-        <PendingReviewParticipantsTable
-            handleSelect={() => {}}
-            people={pendingCompletionReview}
-            selectedPersonId=""
-            small
-            totalParticipants={pendingCompletionReview.count()} />
-        <ViolationsParticipantsTable
-            handleSelect={() => {}}
-            people={violationsWatch}
-            selectedPersonId=""
-            small
-            totalParticipants={violationsWatch.count()} />
-      </RightWrapper>
-    </DashboardBody>
-  </DashboardWrapper>
-);
+class DashboardContainer extends Component<Props, State> {
+  constructor(props :Props) {
+    super(props);
 
-export default Dashboard;
+    this.state = {
+      newParticipants: List(),
+      pendingCompletionReview: List(),
+      violationsWatch: List(),
+    };
+  }
+
+  componentWillReceiveProps() {
+    this.getNewParticipants();
+    this.getParticipantsWithHoursComplete();
+    this.getParticipantsWithViolations();
+  }
+
+  componentDidUpdate() {
+    const { resetRequestState } = this.props;
+    resetRequestState(GET_PARTICIPANTS);
+  }
+
+  getNewParticipants = () => {
+    const { enrollmentByParticipant, participants } = this.props;
+  }
+
+  getParticipantsWithHoursComplete = () => {
+  }
+
+  getParticipantsWithViolations = () => {
+  }
+
+  render() {
+    const {
+      newParticipants,
+      pendingCompletionReview,
+      violationsWatch,
+    } = this.state;
+    const {
+      getParticipantsRequestState,
+      infractionsByParticipant,
+      participants,
+      sentencesByParticipant,
+    } = this.props;
+
+    if (getParticipantsRequestState === RequestStates.PENDING) {
+      return (
+        <LogoLoader
+            loadingText="Please wait..."
+            size={60} />
+      );
+    }
+
+    return (
+      <DashboardWrapper>
+        <DashboardBody>
+          <NewParticipantsTable
+              handleSelect={() => {}}
+              people={newParticipants}
+              sentences={sentencesByParticipant}
+              selectedPersonId=""
+              small
+              totalParticipants={newParticipants.count()} />
+          <RightWrapper>
+            <PendingReviewParticipantsTable
+                handleSelect={() => {}}
+                people={pendingCompletionReview}
+                selectedPersonId=""
+                small
+                totalParticipants={pendingCompletionReview.count()} />
+            <ViolationsParticipantsTable
+                handleSelect={() => {}}
+                people={violationsWatch}
+                selectedPersonId=""
+                small
+                totalParticipants={violationsWatch.count()}
+                violations={infractionsByParticipant} />
+          </RightWrapper>
+        </DashboardBody>
+      </DashboardWrapper>
+    );
+  }
+}
+
+export default DashboardContainer;
