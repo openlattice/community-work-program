d01f1c3236574302c425b70d2e7b5bb9a3ce8673
Giulia Campana <giuliaecampana@gmail.com>
2019-05-31 15:35:03 -0700

    Add missing SequenceAction type to AppReducer

--- src/containers/app/AppReducer.js
+++ src/containers/app/AppReducer.js
@@ -6,6 +6,7 @@ import { Models } from 'lattice';
 import { AccountUtils } from 'lattice-auth';
 import isNumber from 'lodash/isNumber';
 import { Map, fromJS } from 'immutable';
+import type { SequenceAction } from 'redux-reqseq';
 
 import { APP } from '../../utils/constants/ReduxStateConsts';
 import { isDefined } from '../../utils/LangUtils';
@@ -83,12 +84,6 @@ export default function appReducer(state :Map<*, *> = INITIAL_STATE, action :Obj
 
           const seqAction :SequenceAction = action;
           const error = {};
-
-          /*
-           * value is expected to be an error object. for lattice-sagas / lattice-js, the error object is expected
-           * to be the Axios error object. for more info:
-           *   https://github.com/axios/axios#handling-errors
-           */
           const { value: axiosError } = seqAction;
           if (axiosError && axiosError.response && isNumber(axiosError.response.status)) {
             // for now, we only care about the HTTP status code. we can get more fancy later on.
