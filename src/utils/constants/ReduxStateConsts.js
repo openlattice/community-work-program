534331ede0eedf2cac5e1f5fe65b0f7e49b000d7
Giulia <giuliaecampana@gmail.com>
2019-06-10 15:16:38 -0700

    Add ReduxStateConsts

--- src/utils/constants/ReduxStateConsts.js
+++ src/utils/constants/ReduxStateConsts.js
@@ -1,5 +1,6 @@
 export const STATE = {
   APP: 'app',
+  EDM: 'edm',
   PEOPLE: 'people',
 };
 
@@ -8,6 +9,7 @@ export const STATE = {
 export const APP = {
   ACTIONS: 'actions',
   APP: 'app',
+  APP_SETTINGS_ID: 'appSettingsEntitySetId',
   APP_TYPES: 'appTypes',
   ENTITY_SETS_BY_ORG: 'entitySetsByOrganization',
   FQN_TO_ID: 'fqnsToEntitySetIds',
@@ -17,9 +19,9 @@ export const APP = {
   ORGS: 'organizations',
   PRIMARY_KEYS: 'primaryKeys',
   PROPERTY_TYPES: 'propertyTypes',
+  REQUEST_STATE: 'requestState',
   SELECTED_ORG_ID: 'selectedOrganizationId',
   SELECTED_ORG_TITLE: 'selectedOrganizationTitle',
-  APP_SETTINGS_ID: 'appSettingsEntitySetId',
   SELECTED_ORG_SETTINGS: 'selectedOrganizationSettings',
   SETTINGS_BY_ORG_ID: 'appSettingsByOrgId'
 };
@@ -28,7 +30,19 @@ export const APP = {
 
 export const PEOPLE = {
   ACTIONS: 'actions',
+  ENROLLMENT_BY_PARTICIPANT: 'enrollmentByParticipant',
+  ERRORS: 'errors',
+  GET_ENROLLMENT_STATUSES: 'getEnrollmentStatuses',
+  GET_HOURS_WORKED: 'getHoursWorked',
+  GET_INFRACTIONS: 'getInfractions',
   GET_PARTICIPANTS: 'getParticipants',
-  IS_FETCHING_PARTICIPANTS: 'isFetchingParticipants',
+  GET_SENTENCE_TERMS: 'getSentenceTerms',
+  GET_SENTENCES: 'getSentences',
+  HOURS_WORKED: 'hoursWorked',
+  INFRACTIONS_BY_PARTICIPANT: 'infractionsByParticipant',
+  INFRACTION_COUNTS_BY_PARTICIPANT: 'infractionCountsByParticipant',
   PARTICIPANTS: 'participants',
+  REQUEST_STATE: 'requestState',
+  SENTENCE_TERMS_BY_PARTICIPANT: 'sentenceTermsByParticipant',
+  SENTENCES: 'sentences',
 };
