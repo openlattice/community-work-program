bc841ada9c87871a700cfc3d80ba7111e012bc43
Giulia <giuliaecampana@gmail.com>
2019-06-10 18:17:33 -0700

    Add array for Participants table column headers

---
+++ src/utils/constants/UIConsts.js
@@ -0,0 +1,14 @@
+// @flow
+
+/* Dashboard */
+
+export const allParticipantsColumns = [
+  'NAME',
+  'AGE',
+  'START DATE',
+  'SENT. DATE',
+  'SENT. END DATE',
+  '# OF WARN.',
+  '# OF VIO.',
+  'HRS. SERVED'
+];
