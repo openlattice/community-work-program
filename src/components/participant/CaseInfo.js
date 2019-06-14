77da11b6d5a78057d601166048573aaa0918e6c5
Giulia <giuliaecampana@gmail.com>
2019-06-13 18:37:54 -0700

    add other profile components that don't yet work with real data

---
+++ src/components/participant/CaseInfo.js
@@ -0,0 +1,67 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { faPenSquare } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { ButtonWrapper } from '../Layout';
+import { OL } from '../../core/style/Colors';
+
+const CaseInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 258px;
+  height: 200px;
+  border: 1px solid ${OL.GREY11};
+  background-color: ${OL.WHITE};
+  padding: 20px 0;
+  border-radius: 5px;
+`;
+
+const Header = styled.div`
+  color: ${OL.GREY02};
+  font-weight: 600;
+  font-size: 16px;
+  margin: 8px;
+`;
+
+const Number = styled.span`
+  color: ${OL.BLACK};
+  font-size: 18px;
+  font-weight: 600;
+  margin: 0 8px 0 0
+`;
+
+const NumberWrapper = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${OL.GREY08};
+  padding-bottom: 15px;
+  :last-of-type {
+    border: none;
+    padding-bottom: 0;
+  }
+`;
+
+type Props = {
+  edit :() => void;
+};
+
+const CaseInfo = ({ edit } :Props) => (
+  <CaseInfoWrapper>
+    <Header>Case Number</Header>
+    <NumberWrapper>
+      <Number>1234567890123456</Number>
+      <ButtonWrapper onClick={edit}>
+        <FontAwesomeIcon icon={faPenSquare} size="lg" color={OL.GREY04} />
+      </ButtonWrapper>
+    </NumberWrapper>
+    <Header>Required Hours</Header>
+    <Number>100</Number>
+  </CaseInfoWrapper>
+);
+
+export default CaseInfo;
---
+++ src/components/participant/GeneralInfo.js
@@ -0,0 +1,151 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { Map } from 'immutable';
+import { faPenSquare, faUserCircle } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter } from 'react-router-dom';
+
+import { OL } from '../../core/style/Colors';
+import { PersonPhoto, PersonPicture } from '../picture/PersonPicture';
+import { ButtonWrapper } from '../Layout';
+import { formatAsDate } from '../../utils/DateTimeUtils';
+import { getEntityProperties } from '../../utils/DataUtils';
+import { PEOPLE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
+
+const { DOB } = PEOPLE_FQNS;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 400px;
+  border: 1px solid ${OL.GREY11};
+  background-color: ${OL.WHITE};
+  padding: 30px;
+  border-radius: 5px;
+`;
+
+const StyledPersonPhoto = styled(PersonPhoto)`
+  height: 100px;
+  width: 100px;
+  margin: 0 0 30px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledPlaceholderPicture = styled.div`
+  margin: 0 0 30px 0;
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 8px 0;
+  padding: 0 0 8px 0;
+  border-bottom: 1px solid ${OL.GREY08};
+  width: 100%;
+  font-size: 14px;
+  :last-of-type {
+    border: none;
+  }
+`;
+
+const Title = styled.div`
+  color: ${OL.GREY02};
+  font-weight: 600;
+  text-align: left;
+  width: 40%;
+`;
+
+const Value = styled.div`
+  text-align: left;
+  width: 60%;
+  font-weight: ${props => props.fontWeight};
+  color: ${(props) => {
+    if (props.status === 'Active') {
+      return `${OL.GREEN02};`;
+    }
+    if (props.status === 'Completed') {
+      return `${OL.BLUE02};`;
+    }
+    if (props.status === 'Active – noncompliant') {
+      return `${OL.YELLOW01};`;
+    }
+    if (props.status === 'Removed – noncompliant') {
+      return `${OL.RED01};`;
+    }
+    if (props.status === 'Awaiting enrollment') {
+      return `${OL.PURPLE03};`;
+    }
+    return `${OL.GREY01};`;
+  }}
+`;
+
+const EmailWrapper = styled(Value)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+type Props = {
+  person :Map;
+};
+
+// <ButtonWrapper>
+//   <FontAwesomeIcon icon={faPenSquare} size="lg" color={OL.GREY04} />
+// </ButtonWrapper>
+
+const GeneralInfo = ({ person } :Props) => {
+
+  const { [DOB]: dateOfBirth } = getEntityProperties(person, [DOB]);
+  const dob = formatAsDate(dateOfBirth);
+  return (
+    <InfoWrapper>
+      {
+        person && person.get('mugshot')
+          ? (
+            <StyledPersonPhoto>
+              <PersonPicture src={person.get('mugshot')} alt="" />
+            </StyledPersonPhoto>
+          )
+          : (
+            <StyledPlaceholderPicture>
+              <FontAwesomeIcon icon={faUserCircle} size="6x" color="#D8D8D8" />
+            </StyledPlaceholderPicture>
+          )
+      }
+      <InfoRow>
+        <Title>Status</Title>
+        <Value
+            status={person.get('status')}
+            fontWeight={600}>
+          {person.get('status')}
+        </Value>
+      </InfoRow>
+      <InfoRow>
+        <Title>Date of Birth</Title>
+        <Value>{dob}</Value>
+      </InfoRow>
+      <InfoRow>
+        <Title>Phone #</Title>
+        <Value></Value>
+      </InfoRow>
+      <InfoRow>
+        <Title>Address</Title>
+        <Value></Value>
+      </InfoRow>
+      <InfoRow>
+        <Title>Email</Title>
+        <EmailWrapper>
+        </EmailWrapper>
+      </InfoRow>
+    </InfoWrapper>
+  );
+}
+
+export default withRouter(GeneralInfo);
