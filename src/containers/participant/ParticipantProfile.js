79a0103724f2c493fe23b75e307d443ddfcd485b
Giulia <giuliaecampana@gmail.com>
2019-06-13 17:05:25 -0700

    Remove not yet necessary profile components

--- src/containers/participant/ParticipantProfile.js
+++ src/containers/participant/ParticipantProfile.js
@@ -9,11 +9,12 @@ import { PARTICIPANT_PROFILE_WIDTH } from '../../core/style/Sizes';
 import { PARTICIPANTS } from '../../core/router/Routes';
 import {
   BackNavButton,
-  PrimaryButton,
 } from '../../components/controls/index';
+import { getEntityProperties } from '../../utils/DataUtils';
 import { PEOPLE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 
 const { FIRST_NAME, LAST_NAME } = PEOPLE_FQNS;
+const { PARTICIPANT } = PERSON;
 
 const ProfileWrapper = styled.div`
   display: flex;
@@ -47,59 +48,20 @@ const NameHeader = styled.div`
   color: ${OL.BLACK};
 `;
 
-const BasicInfoWrapper = styled.div`
-  margin-top: 15px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const InnerColumnWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const InnerRowWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-const ModalBodyWrapper = styled.div`
-  width: 400px;
-  height: 110px;
-  display: grid;
-`;
-
-const ActionWrapper = styled.div`
-  position: fixed;
-  width: 400px;
-  margin-top: 85px;
-  margin-bottom: 20px;
-  z-index: 200;
-`;
-
-const ModalText = styled.div`
-  font-size: 14px;
-  color: ${OL.GREY02};
-  margin: 5px 0;
-  display: block;
-`;
-
 type Props = {
   history :RouterHistory;
-  person :Map;
+  personEKID :string;
 };
 
 type State = {
 };
-// eslint-disable-next-line react/prefer-stateless-function
+
 class ParticipantProfile extends Component<Props, State> {
   render() {
-    const { history, person } = this.props;
-    console.log('person: ', person);
+    const { history, participant } = this.props;
+    const { [FIRST_NAME]: firstName, [LAST_NAME]: lastName } = getEntityProperties(
+      participant, [FIRST_NAME, LAST_NAME]
+    );
     return (
       <ProfileWrapper>
         <ProfileBody>
@@ -109,30 +71,14 @@ class ParticipantProfile extends Component<Props, State> {
               }}>
             Back to Participants
           </BackNavButton>
+          <NameRowWrapper>
+            <NameHeader>{ `${firstName} ${lastName}` }</NameHeader>
+          </NameRowWrapper>
         </ProfileBody>
       </ProfileWrapper>
     );
   }
 }
 
-// <NameRowWrapper>
-//   <NameHeader>{ `${person.getIn([FIRST_NAME, 0])} ${person.getIn([LAST_NAME, 0])}` }</NameHeader>
-// </NameRowWrapper>
-
-// <PrimaryButton>
-//   {buttonText}
-// </PrimaryButton>
 
 
-// <BasicInfoWrapper>
-//   <GeneralInfo person={person} />
-//   <InnerColumnWrapper>
-//     <KeyDates
-//         person={person} />
-//     <InnerRowWrapper>
-//       <CaseInfo />
-//       <ViolationsDisplay />
-//     </InnerRowWrapper>
-//   </InnerColumnWrapper>
-// </BasicInfoWrapper>
-export default ParticipantProfile;
