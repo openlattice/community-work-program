32bff0031f828ee46b8e85d57bff9cd5895b8aca
Giulia <giuliaecampana@gmail.com>
2019-06-14 17:20:54 -0700

    update controls index

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -5,11 +5,6 @@ import StyledButton from './buttons/StyledButton';
 import TertiaryButton from './buttons/TertiaryButton';
 import TertiaryButtonLink from './buttons/TertiaryButtonLink';
 
-import StyledFunctionSelect from './dropdowns/StyledFunctionSelect';
-import StyledSelect from './dropdowns/StyledSelect';
-
-import StyledInput from './input/StyledInput';
-
 import ToolBar from './ToolBar';
 
 export {
@@ -17,9 +12,6 @@ export {
   NavButton,
   PrimaryButton,
   StyledButton,
-  StyledFunctionSelect,
-  StyledInput,
-  StyledSelect,
   TertiaryButton,
   TertiaryButtonLink,
   ToolBar,
