48c68794ad54df1328ed41338b03b0c3b2ea8a80
Giulia <giuliaecampana@gmail.com>
2019-06-19 15:14:22 -0700

    Add phone number formatting util function

--- src/utils/FormattingUtils.js
+++ src/utils/FormattingUtils.js
@@ -12,3 +12,17 @@ export function formatNumericalValue(rawValue :number) :string {
   if (!rawValue === undefined) return '';
   return (rawValue).toString();
 }
+
+export const formatPhoneNumber = (rawValue :string) => {
+  if (!rawValue === undefined) return '';
+  const cleaned = (rawValue).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  const matchPlusOne = cleaned.match(/^(\d{1})(\d{3})(\d{3})(\d{4})$/);
+  if (matchPlusOne) {
+    return `(${matchPlusOne[2]}) ${matchPlusOne[3]}-${matchPlusOne[4]}`;
+  }
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+  return null;
+};
