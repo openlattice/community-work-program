1138177d9cdddef92e1e082b4b50b4e4e6fc41d5
Giulia <giuliaecampana@gmail.com>
2019-06-21 09:52:02 -0700

    Use getStyleVariation from LUK

--- src/components/participant/GeneralInfo.js
+++ src/components/participant/GeneralInfo.js
@@ -2,21 +2,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Map } from 'immutable';
-import { Spinner } from 'lattice-ui-kit';
+import { StyleUtils, Spinner } from 'lattice-ui-kit';
 import { faUserCircle } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { withRouter } from 'react-router-dom';
 import { RequestStates } from 'redux-reqseq';
 import type { RequestState } from 'redux-reqseq';
 
-import { OL } from '../../core/style/Colors';
+import { ENROLLMENT_STATUS_COLORS, OL } from '../../core/style/Colors';
 import { PersonPhoto, PersonPicture } from '../picture/PersonPicture';
 import { formatAsDate } from '../../utils/DateTimeUtils';
 import { getEntityProperties } from '../../utils/DataUtils';
 import { PEOPLE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 import { ENROLLMENT_STATUSES } from '../../core/edm/constants/DataModelConsts';
 
+const { getStyleVariation } = StyleUtils;
 const { DOB, MUGSHOT } = PEOPLE_FQNS;
+
+const statusColorVariation = getStyleVariation('status', {
+  default: `${OL.GREY02}`,
+  [ENROLLMENT_STATUSES.ACTIVE]: ENROLLMENT_STATUS_COLORS.ACTIVE,
+  [ENROLLMENT_STATUSES.ACTIVE_NONCOMPLIANT]: ENROLLMENT_STATUS_COLORS.ACTIVE_NONCOMPLIANT,
+  [ENROLLMENT_STATUSES.AWAITING_ENROLLMENT]: ENROLLMENT_STATUS_COLORS.AWAITING_ENROLLMENT,
+  [ENROLLMENT_STATUSES.COMPLETED]: ENROLLMENT_STATUS_COLORS.COMPLETED,
+  [ENROLLMENT_STATUSES.REMOVED_NONCOMPLIANT]: ENROLLMENT_STATUS_COLORS.REMOVED_NONCOMPLIANT,
+});
 
 const InfoWrapper = styled.div`
   display: flex;
@@ -76,24 +86,7 @@ const Value = styled.div`
   text-align: left;
   width: ${props => (props.hasSpinner ? '100%' : '60%')};
   font-weight: ${props => props.fontWeight};
-  color: ${(props) => {
-    if (props.status === ENROLLMENT_STATUSES.ACTIVE) {
-      return `${OL.GREEN02};`;
-    }
-    if (props.status === ENROLLMENT_STATUSES.COMPLETED) {
-      return `${OL.BLUE01};`;
-    }
-    if (props.status === ENROLLMENT_STATUSES.ACTIVE_NONCOMPLIANT) {
-      return `${OL.YELLOW01};`;
-    }
-    if (props.status === ENROLLMENT_STATUSES.REMOVED_NONCOMPLIANT) {
-      return `${OL.RED01};`;
-    }
-    if (props.status === ENROLLMENT_STATUSES.AWAITING_ENROLLMENT) {
-      return `${OL.PINK01};`;
-    }
-    return `${OL.GREY02};`;
-  }}
+  color: ${statusColorVariation};
 `;
 
 type Props = {
