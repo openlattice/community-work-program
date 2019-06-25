819579e35c067be51e6a73e1c2b42f5544033a4c
Giulia <giuliaecampana@gmail.com>
2019-06-24 19:25:37 -0700

    Do not call getSentences in AppContainer

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -5,11 +5,10 @@
 import React, { Component } from 'react';
 
 import styled from 'styled-components';
-import { Map } from 'immutable';
 import { AuthActions } from 'lattice-auth';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import type { RequestSequence } from 'redux-reqseq';
 
 import AppHeaderContainer from './AppHeaderContainer';
@@ -22,8 +21,6 @@ import * as Routes from '../../core/router/Routes';
 import {
   APP_CONTAINER_WIDTH,
 } from '../../core/style/Sizes';
-import { APP, STATE } from '../../utils/constants/ReduxStateConsts';
-import { APP_TYPE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 import { OL } from '../../core/style/Colors';
 
 const { logout } = AuthActions;
@@ -55,12 +52,10 @@ const AppContentInnerWrapper = styled.div`
 
 type Props = {
   actions:{
-    getSentences :RequestSequence;
     initializeApplication :RequestSequence;
     logout :() => void;
     resetRequestState :(actionType :string) => void;
   },
-  app :Map<*, *>,
 };
 
 class AppContainer extends Component<Props> {
@@ -72,30 +67,9 @@ class AppContainer extends Component<Props> {
     actions.initializeApplication();
   }
 
-  componentDidUpdate(prevProps :Props) {
-    const { app, actions } = this.props;
-    const nextOrg = app.get(APP.ORGS);
-    const prevOrg = prevProps.app.get(APP.ORGS);
-    if (prevOrg.size !== nextOrg.size) {
-      nextOrg.keySeq().forEach((id) => {
-        const selectedOrgId :UUID = id;
-        const peopleESID :UUID = app.getIn(
-          [APP_TYPE_FQNS.PEOPLE, APP.ENTITY_SETS_BY_ORG, selectedOrgId]
-        );
-        if (peopleESID) {
-          actions.getSentences();
-        }
-      });
-    }
-  }
-
-  renderParticipantsContent = () => (
-    <ParticipantsRouter />
-  );
-
   renderAppContent = () => (
     <Switch>
-      { this.renderParticipantsContent() }
+      <ParticipantsRouter />
       <Route path={Routes.WORKSITES} component={Worksites} />
     </Switch>
   );
@@ -115,16 +89,8 @@ class AppContainer extends Component<Props> {
   }
 }
 
-const mapStateToProps = (state :Map<*, *>) => {
-  const app = state.get(STATE.APP);
-  return {
-    app,
-  };
-};
-
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({
-    getSentences: ParticipantsActions.getSentences,
     initializeApplication: AppActions.initializeApplication,
     logout,
     resetRequestState: ParticipantsActions.resetRequestState,
@@ -132,4 +98,4 @@ const mapDispatchToProps = dispatch => ({
 });
 
 // $FlowFixMe
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AppContainer));
+export default withRouter(connect(null, mapDispatchToProps)(AppContainer));
