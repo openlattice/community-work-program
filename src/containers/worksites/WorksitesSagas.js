dda5a6d9b1be60103848e87474b0768b596f294e
Giulia <giuliaecampana@gmail.com>
2019-07-02 13:20:45 -0700

    Flesh out getWorksitePlans saga to get relevant info for worksites tables

--- src/containers/worksites/WorksitesSagas.js
+++ src/containers/worksites/WorksitesSagas.js
@@ -17,9 +17,15 @@ import type { SequenceAction } from 'redux-reqseq';
 
 import Logger from '../../utils/Logger';
 import { ERR_ACTION_VALUE_NOT_DEFINED } from '../../utils/Errors';
-import { getEntitySetIdFromApp, getNeighborDetails } from '../../utils/DataUtils';
+import { WORKSITE_INFO_CONSTS } from './WorksitesConstants';
+import {
+  getEntityKeyId,
+  getEntityProperties,
+  getEntitySetIdFromApp,
+  getNeighborDetails
+} from '../../utils/DataUtils';
 import { STATE } from '../../utils/constants/ReduxStateConsts';
-import { APP_TYPE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
+import { APP_TYPE_FQNS, WORKSITE_PLAN_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 import {
   GET_WORKSITES,
   GET_WORKSITE_PLANS,
@@ -27,7 +33,9 @@ import {
   getWorksites,
 } from './WorksitesActions';
 
+const { PAST, SCHEDULED, TOTAL_HOURS } = WORKSITE_INFO_CONSTS;
 const { ORGANIZATION, WORKSITE, WORKSITE_PLAN } = APP_TYPE_FQNS;
+const { HOURS_WORKED, REQUIRED_HOURS } = WORKSITE_PLAN_FQNS;
 
 const LOG = new Logger('WorksitesSagas');
 const { searchEntityNeighborsWithFilter } = SearchApiActions;
@@ -53,6 +61,9 @@ function* getWorksitePlansWorker(action :SequenceAction) :Generator<*, *, *> {
     if (value === null || value === undefined) {
       throw ERR_ACTION_VALUE_NOT_DEFINED;
     }
+    /*
+     * 1. For each worksite given, get all associated worksite plans.
+     */
     const { worksiteEKIDs } = value;
     const app = yield select(getAppFromState);
     const worksiteESID = getEntitySetIdFromApp(app, WORKSITE);
@@ -74,7 +85,52 @@ function* getWorksitePlansWorker(action :SequenceAction) :Generator<*, *, *> {
       .map((worksiteList :List) => worksiteList
         .map((worksite :Map) => getNeighborDetails(worksite)));
 
-    yield put(getWorksitePlans.success(id, worksitePlansByWorksite));
+    /*
+     * 2. For each worksite with worksite plans, calculate the number of scheduled and past participants,
+          as well as total hours worked so far at the worksite.
+     */
+    const worksitePlanCount :number = worksitePlansByWorksite.count() ? worksitePlansByWorksite
+      .reduce((totalCount, worksitePlanList) => (totalCount + worksitePlanList.count()), 0) : 0;
+
+    let worksitePlanInfoMap :Map = Map();
+    if (worksitePlanCount) {
+      worksitePlansByWorksite.forEach((worksitePlans :List, worksiteEKID :UUID) => {
+
+        const scheduledParticipants :number = worksitePlans.reduce((total, plan) => {
+          const {
+            [HOURS_WORKED]: hoursWorked,
+            [REQUIRED_HOURS]: reqHours
+          } = getEntityProperties(plan, [HOURS_WORKED, REQUIRED_HOURS]);
+          if (!hoursWorked || !reqHours) return total + 0;
+          if (hoursWorked === reqHours) return total + 0;
+          if (hoursWorked !== reqHours) return total + 1;
+          return total + 0;
+        }, 0);
+        const pastParticipants :number = worksitePlans.reduce((total, plan) => {
+          const {
+            [HOURS_WORKED]: hoursWorked,
+            [REQUIRED_HOURS]: reqHours
+          } = getEntityProperties(plan, [HOURS_WORKED, REQUIRED_HOURS]);
+          if (!hoursWorked || !reqHours) return total + 0;
+          if (hoursWorked === reqHours) return total + 1;
+          if (hoursWorked !== reqHours) return total + 0;
+          return total + 0;
+        }, 0);
+        const totalHoursWorkedAtWorksite :number = worksitePlans.reduce((total, plan) => {
+          const { [HOURS_WORKED]: hoursWorked } = getEntityProperties(plan, [HOURS_WORKED]);
+          if (!hoursWorked) return total + 0;
+          return total + hoursWorked;
+        }, 0);
+        const individualWorksiteInfo :Map = Map().withMutations((map) => {
+          map.set(SCHEDULED, scheduledParticipants);
+          map.set(PAST, pastParticipants);
+          map.set(TOTAL_HOURS, totalHoursWorkedAtWorksite);
+        });
+        worksitePlanInfoMap = worksitePlanInfoMap.set(worksiteEKID, individualWorksiteInfo);
+      });
+    }
+
+    yield put(getWorksitePlans.success(id, worksitePlanInfoMap));
   }
   catch (error) {
     workerResponse.error = error;
@@ -130,6 +186,19 @@ function* getWorksitesWorker(action :SequenceAction) :Generator<*, *, *> {
     worksitesByOrg = fromJS(response.data)
       .map((worksiteList :List) => worksiteList
         .map((worksite :Map) => getNeighborDetails(worksite)));
+
+    const worksiteCount :number = worksitesByOrg.count() ? worksitesByOrg
+      .reduce((totalCount, worksiteList) => (totalCount + worksiteList.count()), 0) : 0;
+
+    if (worksiteCount) {
+      const worksiteEKIDs :string[] = worksitesByOrg
+        .reduce((ekidArray, worksiteList) => {
+          const innerEKIDs = worksiteList
+            .reduce((innerEKIDArray, worksite) => (innerEKIDArray.concat([getEntityKeyId(worksite)])), []);
+          return ekidArray.concat(innerEKIDs);
+        }, []);
+      yield call(getWorksitePlansWorker, getWorksitePlans({ worksiteEKIDs }));
+    }
 
     yield put(getWorksites.success(id, worksitesByOrg));
   }
