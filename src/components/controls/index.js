7529dff03460adfc77560cd89848dc1330f73e6a
Giulia <giuliaecampana@gmail.com>
2019-07-09 14:55:30 -0700

    Clean up controls index

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,10 +1,6 @@
 import BackNavButton from './buttons/BackNavButton';
 import EditButton from './buttons/EditButton';
 import { NavButton } from './buttons/NavButton';
-import PrimaryButton from './buttons/PrimaryButton';
-import StyledButton from './buttons/StyledButton';
-import TertiaryButton from './buttons/TertiaryButton';
-import TertiaryButtonLink from './buttons/TertiaryButtonLink';
 
 import ToolBar from './ToolBar';
 
@@ -12,9 +8,5 @@ export {
   BackNavButton,
   EditButton,
   NavButton,
-  PrimaryButton,
-  StyledButton,
-  TertiaryButton,
-  TertiaryButtonLink,
   ToolBar,
 };
