29d6e3a61561e0f3891f14526fbf02b6654c82a2
Giulia <giuliaecampana@gmail.com>
2019-07-09 18:09:55 -0700

    Remove unnecessary EditButton

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,12 +1,10 @@
 import BackNavButton from './buttons/BackNavButton';
-import EditButton from './buttons/EditButton';
 import { NavButton } from './buttons/NavButton';
 
 import ToolBar from './ToolBar';
 
 export {
   BackNavButton,
-  EditButton,
   NavButton,
   ToolBar,
 };
