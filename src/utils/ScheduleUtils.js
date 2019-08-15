df4d5cdec9307154e7eb711d90ba58b542cad385
Giulia <giuliaecampana@gmail.com>
2019-08-15 16:18:31 -0700

    Create util function that makes a regularly repeating schedule

--- src/utils/ScheduleUtils.js
+++ src/utils/ScheduleUtils.js
@@ -1,11 +1,45 @@
 /*
  * @flow
  */
-import { List, Map } from 'immutable';
-import { DateTime } from 'luxon';
+
+import { DateTime, Interval } from 'luxon';
+
+import { isDefined } from './LangUtils';
 
 export const getCombinedDateTime = (date :string, time :string) => {
   const datetimeString :string = date.concat(' ', time);
   const datetime = DateTime.fromSQL(datetimeString).toISO();
   return datetime;
+};
+
+export const getRegularlyRepeatingAppointments = (
+  startDateTime :string,
+  endDateTime :string,
+  endsOnDate :string,
+  units :string,
+  repetitionInterval :number
+) => {
+
+  const startDateTimeObj :DateTime = DateTime.fromISO(startDateTime);
+  const startTime = startDateTimeObj.toLocaleString(DateTime.TIME_24_SIMPLE);
+  const endDateTimeObj :DateTime = DateTime.fromISO(endDateTime);
+  const endsOnDateObj :DateTime = DateTime.fromISO(endsOnDate);
+
+  const intervalObj :Interval = Interval.fromDateTimes(endDateTimeObj, endsOnDateObj);
+  let intervalInUnitsGiven :number = intervalObj.length(units);
+  console.log('intervalInUnitsGiven: ', intervalInUnitsGiven);
+  if (isDefined(repetitionInterval) && repetitionInterval !== 1) {
+    intervalInUnitsGiven /= repetitionInterval;
+  }
+  console.log('intervalInUnitsGiven: ', intervalInUnitsGiven);
+  const dividedEqually :Interval[] = intervalObj.divideEqually(intervalInUnitsGiven);
+
+  const appointmentDates = [];
+  for (let i = 0; i < dividedEqually.length; i += 1) {
+    const startDate = dividedEqually[i].end.toISODate();
+    const startDateTimeISO = getCombinedDateTime(startDate, startTime);
+    appointmentDates.push({ start: startDateTimeISO, end: dividedEqually[i].end.toISO() });
+  }
+  console.log('appointmentDates: ', appointmentDates);
+  return appointmentDates;
 };
