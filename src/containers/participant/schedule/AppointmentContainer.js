99368ea603d0d29808c45cc2e17b9d65e7182b55
Giulia <giuliaecampana@gmail.com>
2019-08-19 19:13:16 -0700

    Create Check In Details modal that renders when person has been checked in for appointment

--- src/containers/participant/schedule/AppointmentContainer.js
+++ src/containers/participant/schedule/AppointmentContainer.js
@@ -4,11 +4,21 @@ import styled from 'styled-components';
 import { Map } from 'immutable';
 import { DateTime } from 'luxon';
 import { Button, Card, CardSegment } from 'lattice-ui-kit';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/pro-solid-svg-icons';
 
 import CheckInModal from './CheckInModal';
+import CheckInDetailsModal from './CheckInDetailsModal';
 
 import { getEntityKeyId, getEntityProperties } from '../../../utils/DataUtils';
+import { isDefined } from '../../../utils/LangUtils';
 import { DATETIME_END, INCIDENT_START_DATETIME } from '../../../core/edm/constants/FullyQualifiedNames';
+import { PERSON, STATE } from '../../../utils/constants/ReduxStateConsts';
+import { OL } from '../../../core/style/Colors';
+import { ButtonWrapper } from '../../../components/Layout';
+
+const { CHECK_INS_BY_APPOINTMENT } = PERSON;
 
 const InfoWrapper = styled.div`
   display: grid;
@@ -31,18 +41,25 @@ const Text = styled.span`
   font-size: 16px;
 `;
 
+const CheckWrapper = styled(ButtonWrapper)`
+  margin: 0;
+`;
+
 type Props = {
   appointment :Map;
+  checkInsByAppointment :Map;
   worksiteName :string;
 };
 
 type State = {
+  isCheckInDetailsModalVisible :boolean;
   isCheckInModalVisible :boolean;
 };
 
 class AppointmentContainer extends Component<Props, State> {
 
   state = {
+    isCheckInDetailsModalVisible: false,
     isCheckInModalVisible: false,
   };
 
@@ -54,9 +71,17 @@ class AppointmentContainer extends Component<Props, State> {
     this.setState({ isCheckInModalVisible: false });
   }
 
+  showCheckInDetailsModal = () => {
+    this.setState({ isCheckInDetailsModalVisible: true });
+  }
+
+  hideCheckInDetailsModal = () => {
+    this.setState({ isCheckInDetailsModalVisible: false });
+  }
+
   render() {
-    const { appointment, worksiteName } = this.props;
-    const { isCheckInModalVisible } = this.state;
+    const { appointment, checkInsByAppointment, worksiteName } = this.props;
+    const { isCheckInDetailsModalVisible, isCheckInModalVisible } = this.state;
 
     const {
       [DATETIME_END]: datetimeEnd,
@@ -70,6 +95,10 @@ class AppointmentContainer extends Component<Props, State> {
     const endTime :string = DateTime.fromISO(datetimeEnd).toLocaleString(DateTime.TIME_SIMPLE);
     const hours :string = `${startTime} - ${endTime}`;
 
+    const appointmentEKID :UUID = getEntityKeyId(appointment);
+    const checkIn :Map = checkInsByAppointment.get(appointmentEKID);
+    const checkedIn :boolean = isDefined(checkIn);
+
     return (
       <Card>
         <CardSegment padding="sm">
@@ -81,19 +110,40 @@ class AppointmentContainer extends Component<Props, State> {
             <Text>{ worksiteName }</Text>
             <Text>{ hours }</Text>
           </InfoWrapper>
-          <Button
-              onClick={this.showCheckInModal}
-              mode="subtle">
-            Check in
-          </Button>
+          {
+            checkedIn
+              ? (
+                <CheckWrapper onClick={this.showCheckInDetailsModal}>
+                  <FontAwesomeIcon icon={faCheckCircle} color={OL.PURPLE02} size="lg" />
+                </CheckWrapper>
+              )
+              : (
+                <Button
+                    onClick={this.showCheckInModal}
+                    mode="subtle">
+                  Check in
+                </Button>
+              )
+          }
         </CardSegment>
         <CheckInModal
             appointment={appointment}
             isOpen={isCheckInModalVisible}
             onClose={this.hideCheckInModal} />
+        <CheckInDetailsModal
+            checkIn={checkIn}
+            isOpen={isCheckInDetailsModalVisible}
+            onClose={this.hideCheckInDetailsModal} />
       </Card>
     );
   }
 }
 
-export default AppointmentContainer;
+const mapStateToProps = (state :Map) => {
+  const person = state.get(STATE.PERSON);
+  return ({
+    [CHECK_INS_BY_APPOINTMENT]: person.get(CHECK_INS_BY_APPOINTMENT),
+  });
+};
+
+export default connect(mapStateToProps)(AppointmentContainer);
---
+++ src/containers/participant/schedule/CheckInDetailsModal.js
@@ -0,0 +1,74 @@
+// @flow
+import React from 'react';
+import { fromJS, OrderedMap, Map } from 'immutable';
+import { CardSegment, DataGrid, Modal } from 'lattice-ui-kit';
+import { DateTime } from 'luxon';
+
+import { getEntityProperties } from '../../../utils/DataUtils';
+import {
+  CHECK_IN_FQNS,
+  DATETIME_END,
+  DATETIME_START,
+  WORKSITE_PLAN_FQNS
+} from '../../../core/edm/constants/FullyQualifiedNames';
+
+const { CHECKED_IN } = CHECK_IN_FQNS;
+const { HOURS_WORKED } = WORKSITE_PLAN_FQNS;
+
+const labelMap :OrderedMap = OrderedMap({
+  checkedIn: 'Checked in?',
+  date: 'Date',
+  timeIn: 'Time in',
+  timeOut: 'Time out',
+  hoursWorked: 'Hours worked',
+});
+
+type Props = {
+  checkIn :Map;
+  isOpen :boolean;
+  onClose :() => void;
+};
+
+const CheckInDetailsModal = ({
+  checkIn,
+  isOpen,
+  onClose,
+} :Props) => {
+
+  const {
+    [CHECKED_IN]: checkedInBoolean,
+    [DATETIME_START]: dateTimeStart,
+    [DATETIME_END]: dateTimeEnd,
+    [HOURS_WORKED]: hoursWorked,
+  } = getEntityProperties(checkIn, [CHECKED_IN, DATETIME_END, DATETIME_START, HOURS_WORKED]);
+
+  const checkedIn :string = checkedInBoolean ? 'Yes' : 'No';
+  const date :string = DateTime.fromISO(dateTimeStart).toLocaleString(DateTime.DATE_SHORT);
+  const timeIn :string = DateTime.fromISO(dateTimeStart).toLocaleString(DateTime.TIME_SIMPLE);
+  const timeOut :string = DateTime.fromISO(dateTimeEnd).toLocaleString(DateTime.TIME_SIMPLE);
+
+  const data :Map = fromJS({
+    checkedIn,
+    date,
+    hoursWorked,
+    timeIn,
+    timeOut,
+  });
+
+  return (
+    <Modal
+        isVisible={isOpen}
+        onClose={onClose}
+        textTitle="Check In"
+        viewportScrolling>
+      <CardSegment padding="lg" vertical>
+        <DataGrid
+            columns={3}
+            data={data}
+            labelMap={labelMap} />
+      </CardSegment>
+    </Modal>
+  );
+};
+
+export default CheckInDetailsModal;
