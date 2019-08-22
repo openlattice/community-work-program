a5da2540e652b7a8a72f00bf1a1eda4fc857dccc
Giulia <giuliaecampana@gmail.com>
2019-08-22 11:43:03 -0700

    Fix styles

--- src/containers/participant/ParticipantProfile.js
+++ src/containers/participant/ParticipantProfile.js
@@ -134,7 +134,7 @@ const InnerRowWrapper = styled.div`
 
 const ButtonsWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   grid-gap: 0 10px;
 `;
 
