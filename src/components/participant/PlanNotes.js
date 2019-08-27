3323be92a94d69e9204cd38bc8c80b6e1422a1cf
Giulia <giuliaecampana@gmail.com>
2019-08-27 12:17:25 +0200

    get rid of edit on notes for now

--- src/components/participant/PlanNotes.js
+++ src/components/participant/PlanNotes.js
@@ -2,19 +2,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Card, Label } from 'lattice-ui-kit';
-import { faEdit } from '@fortawesome/pro-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { EMPTY_FIELD } from '../../containers/participants/ParticipantsConstants';
 
 const NotesWrapper = styled.div`
   width: 100%;
-`;
-
-const LabelWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 `;
 
 const InnerWrapper = styled.div`
@@ -40,10 +32,7 @@ const PlanNotes = ({ notes } :Props) => {
     <NotesWrapper>
       <Card>
         <InnerWrapper>
-          <LabelWrapper>
-            <Label subtle>Program Notes</Label>
-            <FontAwesomeIcon icon={faEdit} size="sm" />
-          </LabelWrapper>
+          <Label subtle>Program Notes</Label>
           <TextWrapper>{ notesToDisplay }</TextWrapper>
         </InnerWrapper>
       </Card>
