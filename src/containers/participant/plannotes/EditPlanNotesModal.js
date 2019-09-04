e39e90c344a6565cbbb40e85b305f20e3eddb284
Giulia <giuliaecampana@gmail.com>
2019-09-04 14:41:07 -0700

    Create modal with form for editing/viewing plan notes

---
+++ src/containers/participant/plannotes/EditPlanNotesModal.js
@@ -0,0 +1,58 @@
+// @flow
+import React, { Component } from 'react';
+import { Map } from 'immutable';
+import { Modal } from 'lattice-ui-kit';
+import { connect } from 'react-redux';
+import { RequestStates } from 'redux-reqseq';
+import type { RequestState } from 'redux-reqseq';
+
+import EditPlanNotesForm from './EditPlanNotesForm';
+
+import { PERSON, STATE } from '../../../utils/constants/ReduxStateConsts';
+
+const { ACTIONS, EDIT_CASE_AND_HOURS, REQUEST_STATE } = PERSON;
+
+
+type Props = {
+  editPlanNotesRequestState :RequestState;
+  isOpen :boolean;
+  onClose :() => void;
+};
+
+class EditPlanNotesModal extends Component<Props> {
+
+  componentDidUpdate(prevProps :Props) {
+    const { editPlanNotesRequestState, onClose } = this.props;
+    const { editPlanNotesRequestState: prevSumbitState } = prevProps;
+    if (editPlanNotesRequestState === RequestStates.SUCCESS
+      && prevSumbitState === RequestStates.PENDING) {
+      onClose();
+    }
+  }
+
+  render() {
+    const {
+      editPlanNotesRequestState,
+      isOpen,
+      onClose,
+    } = this.props;
+    return (
+      <Modal
+          isVisible={isOpen}
+          onClose={onClose}
+          textTitle="CWP Notes"
+          viewportScrolling>
+        <EditPlanNotesForm
+            isLoading={editPlanNotesRequestState === RequestStates.PENDING}
+            onDiscard={onClose} />
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state :Map) => ({
+  editPlanNotesRequestState: state.getIn([STATE.PERSON, ACTIONS, EDIT_CASE_AND_HOURS, REQUEST_STATE]),
+});
+
+// $FlowFixMe
+export default connect(mapStateToProps)(EditPlanNotesModal);
