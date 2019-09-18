24b35d1f26d0a8a7f8bcb3f025a170443e82438f
Giulia <giuliaecampana@gmail.com>
2019-09-18 11:55:30 -0700

    Create new work schedule tab and component

---
+++ src/containers/workschedule/WorkScheduleContainer.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+const WorkScheduleContainer = () => (
+  <div>Work Schedule</div>
+);
+
+export default WorkScheduleContainer;
