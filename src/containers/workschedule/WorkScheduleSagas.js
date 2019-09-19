7719227573a9a9eb24545f01def46d1737d96164
Giulia <giuliaecampana@gmail.com>
2019-09-19 10:49:27 -0700

    create  WorkScheduleSagas file

---
+++ src/containers/workschedule/WorkScheduleSagas.js
@@ -0,0 +1,30 @@
+/*
+ * @flow
+ */
+
+import { List, Map, fromJS } from 'immutable';
+import {
+  call,
+  put,
+  select,
+  takeEvery,
+} from '@redux-saga/core/effects';
+import {
+  DataApiActions,
+  DataApiSagas,
+  SearchApiActions,
+  SearchApiSagas
+} from 'lattice-sagas';
+import type { SequenceAction } from 'redux-reqseq';
+
+import Logger from '../../utils/Logger';
+import { ERR_ACTION_VALUE_NOT_DEFINED } from '../../utils/Errors';
+
+const LOG = new Logger('WorkScheduleSagas');
+
+/*
+appointment -> addresses -> work site plan -> based on -> work site
+work site plan -> related to -> enrollment status
+
+1. get all work site plans
+*/
