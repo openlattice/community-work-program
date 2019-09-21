98c3897716f3bce44122fe4ded1533296b2c547b
Giulia <giuliaecampana@gmail.com>
2019-09-20 17:25:35 -0700

    add work sites options

--- src/containers/workschedule/WorkScheduleConstants.js
+++ src/containers/workschedule/WorkScheduleConstants.js
@@ -1,7 +1,11 @@
 // @flow
+import startCase from 'lodash/startCase';
 
-export const timePeriods = {
+export const timePeriods :Object = {
   DAY: 'day',
   WEEK: 'week',
   MONTH: 'month'
 };
+
+export const TIME_PERIOD_OPTIONS :Object[] = Object.values(timePeriods)
+  .map((timePeriod :any) => ({ label: startCase(timePeriod), value: timePeriod }));
