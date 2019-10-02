9fd58df8cb6d3d07cc787ff7a4dc4daf57410950
Giulia <giuliaecampana@gmail.com>
2019-10-01 17:10:39 -0700

    redo plan notes

---
+++ src/components/participant/ProgramNotes.js
@@ -0,0 +1,42 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { Card, CardSegment } from 'lattice-ui-kit';
+
+import {
+  SectionLabel,
+  SectionNameRow,
+  SectionWrapper,
+  SmallEditButton,
+} from './SectionStyledComponents';
+import { EMPTY_FIELD } from '../../containers/participants/ParticipantsConstants';
+
+const NotesCard = styled(Card)`
+  height: 70px;
+`;
+
+const TextWrapper = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+type Props = {
+  notes :string;
+};
+
+const ProgramNotes = ({ notes } :Props) => (
+  <SectionWrapper>
+    <SectionNameRow>
+      <SectionLabel subtle>Program Notes</SectionLabel>
+      <SmallEditButton mode="subtle" onClick={() => {}} />
+    </SectionNameRow>
+    <NotesCard>
+      <CardSegment padding="md">
+        <TextWrapper>{ notes || EMPTY_FIELD }</TextWrapper>
+      </CardSegment>
+    </NotesCard>
+  </SectionWrapper>
+);
+
+export default ProgramNotes;
