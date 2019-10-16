c855ea47e510daaf1fd27c10f626aedc4270b42e
Giulia <giuliaecampana@gmail.com>
2019-10-16 16:16:50 -0700

    EditWorksiteForm

---
+++ src/containers/worksites/EditWorksiteForm.js
@@ -0,0 +1,131 @@
+// @flow
+import React, { Component } from 'react';
+import { Map } from 'immutable';
+import { DateTime } from 'luxon';
+import { Card, CardHeader } from 'lattice-ui-kit';
+import { Form, DataProcessingUtils } from 'lattice-fabricate';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { RequestSequence } from 'redux-reqseq';
+
+import { editWorksite } from './WorksitesActions';
+import { APP_TYPE_FQNS, WORKSITE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
+import { worksiteSchema, worksiteUiSchema } from './schemas/EditWorksiteInfoSchemas';
+import { getEntityProperties } from '../../utils/DataUtils';
+
+const { WORKSITE } = APP_TYPE_FQNS;
+const {
+  DATETIME_END,
+  DATETIME_START,
+  DESCRIPTION,
+  NAME,
+} = WORKSITE_FQNS;
+
+const {
+  getEntityAddressKey,
+  getPageSectionKey,
+} = DataProcessingUtils;
+
+type Props = {
+  actions:{
+    editWorksite :RequestSequence;
+  },
+  entityIndexToIdMap :Map;
+  entitySetIds :Object;
+  propertyTypeIds :Object;
+  worksite :Map;
+};
+
+type State = {
+  formData :Object;
+  prepopulated :boolean;
+};
+
+class EditWorksiteForm extends Component<Props, State> {
+
+  state = {
+    formData: {},
+    prepopulated: false,
+  };
+
+  componentDidMount() {
+    this.prepopulateFormData();
+  }
+
+  componentDidUpdate(prevProps :Props) {
+    const { worksite } = this.props;
+    if (!prevProps.worksite.equals(worksite)) {
+      this.prepopulateFormData();
+    }
+  }
+
+  prepopulateFormData = () => {
+    const { worksite } = this.props;
+
+    const prepopulated = !worksite.isEmpty();
+    const formData = {};
+    if (prepopulated) {
+      const {
+        [DATETIME_END]: dateInactive,
+        [DATETIME_START]: dateActive,
+        [DESCRIPTION]: availableWork,
+        [NAME]: worksiteName
+      } = getEntityProperties(worksite, [DATETIME_END, DATETIME_START, DESCRIPTION, NAME]);
+
+      const sectionOneKey = getPageSectionKey(1, 1);
+      formData[sectionOneKey] = {};
+      formData[sectionOneKey][getEntityAddressKey(0, WORKSITE, NAME)] = worksiteName;
+      formData[sectionOneKey][getEntityAddressKey(0, WORKSITE, DATETIME_START)] = DateTime
+        .fromISO(dateActive).toISODate() || '';
+      formData[sectionOneKey][getEntityAddressKey(0, WORKSITE, DATETIME_END)] = DateTime
+        .fromISO(dateInactive).toISODate() || '';
+      formData[sectionOneKey][getEntityAddressKey(0, WORKSITE, DESCRIPTION)] = availableWork;
+    }
+
+    this.setState({
+      formData,
+      prepopulated,
+    });
+  }
+
+  render() {
+    const {
+      actions,
+      entityIndexToIdMap,
+      entitySetIds,
+      propertyTypeIds,
+    } = this.props;
+    const {
+      formData,
+      prepopulated,
+    } = this.state;
+
+    const formContext = {
+      editAction: actions.editWorksite,
+      entityIndexToIdMap,
+      entitySetIds,
+      propertyTypeIds,
+    };
+
+    return (
+      <Card>
+        <CardHeader padding="sm">Edit Worksite Info</CardHeader>
+        <Form
+            disabled={prepopulated}
+            formContext={formContext}
+            formData={formData}
+            schema={worksiteSchema}
+            uiSchema={worksiteUiSchema} />
+      </Card>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({
+    editWorksite,
+  }, dispatch)
+});
+
+// $FlowFixMe
+export default connect(null, mapDispatchToProps)(EditWorksiteForm);
