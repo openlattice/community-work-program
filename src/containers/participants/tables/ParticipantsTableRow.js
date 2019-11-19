2d7012a7fb8bc08d0dccf85bd5c0e629cb881f27
Giulia <giuliaecampana@gmail.com>
2019-11-19 10:44:08 -0800

    add an onClick for the table rows

--- src/containers/participants/tables/ParticipantsTableRow.js
+++ src/containers/participants/tables/ParticipantsTableRow.js
@@ -1,22 +1,18 @@
 // @flow
 import React, { Component } from 'react';
-import { List, Map } from 'immutable';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import type { RowData } from 'lattice-ui-kit';
 import type { RequestSequence } from 'redux-reqseq';
 
-import { getEntityKeyId } from '../../../utils/DataUtils';
-import { isDefined } from '../../../utils/LangUtils';
-import { PERSON, STATE } from '../../../utils/constants/ReduxStateConsts';
+import { goToRoute } from '../../../core/router/RoutingActions';
+import * as Routes from '../../../core/router/Routes';
 import { StyledParticipantsRow } from './styled/index';
-
-const { ALL_DIVERSION_PLANS } = PERSON;
 
 type Props = {
   actions:{
-    getEnrollmentFromDiversionPlan :RequestSequence;
+    goToRoute :RequestSequence;
   };
-  allDiversionPlans :List;
   className ? :string;
   components :Object;
   data :RowData;
@@ -29,13 +25,10 @@ class ParticipantsTableRow extends Component<Props> {
     className: undefined
   }
 
-  selectDiversionPlan = (ekid :string) => {
-    const { actions, allDiversionPlans } = this.props;
-    const diversionPlan :Map = allDiversionPlans.find((plan :Map) => getEntityKeyId(plan) === ekid);
-
-    if (isDefined(diversionPlan) && !diversionPlan.isEmpty()) {
-      actions.getEnrollmentFromDiversionPlan({ diversionPlan });
-    }
+  goToPersonProfile = () => {
+    const { actions, data } = this.props;
+    const { id: personEKID } = data;
+    actions.goToRoute(Routes.PARTICIPANT_PROFILE.replace(':subjectId', personEKID));
   }
 
   render() {
@@ -52,26 +45,24 @@ class ParticipantsTableRow extends Component<Props> {
       .map((header) => (
         <components.Cell
             key={`${id}_cell_${header.key}`}
-            onClick={() => this.selectDiversionPlan(id)}
             status={data[header.key]}>
           {data[header.key]}
         </components.Cell>
       ));
 
     return (
-      <StyledParticipantsRow className={className}>
+      <StyledParticipantsRow className={className} onClick={this.goToPersonProfile}>
         {cells}
       </StyledParticipantsRow>
     );
   }
 }
 
-const mapStateToProps = (state :Map<*, *>) => {
-  const person = state.get(STATE.PERSON);
-  return {
-    [ALL_DIVERSION_PLANS]: person.get(ALL_DIVERSION_PLANS),
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({
+    goToRoute,
+  }, dispatch)
+});
 
 // $FlowFixMe
-export default connect(mapStateToProps)(ParticipantsTableRow);
+export default connect(null, mapDispatchToProps)(ParticipantsTableRow);
