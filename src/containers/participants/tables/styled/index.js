9392610880ca9977ccc89ef8d99590f53f4dd694
Giulia <giuliaecampana@gmail.com>
2019-11-18 17:06:11 -0800

    table styled components

---
+++ src/containers/participants/tables/styled/index.js
@@ -0,0 +1,91 @@
+// @flow
+import styled from 'styled-components';
+import { StyleUtils } from 'lattice-ui-kit';
+
+import { ENROLLMENT_STATUSES } from '../../../../core/edm/constants/DataModelConsts';
+import { ENROLLMENT_STATUS_COLORS, OL } from '../../../../core/style/Colors';
+
+const { getStickyPosition, getStyleVariation } = StyleUtils;
+
+const statusColorVariation = getStyleVariation('status', {
+  default: OL.GREY02,
+  [ENROLLMENT_STATUSES.ACTIVE]: ENROLLMENT_STATUS_COLORS.ACTIVE,
+  [ENROLLMENT_STATUSES.ACTIVE_REOPENED]: ENROLLMENT_STATUS_COLORS.ACTIVE_REOPENED,
+  [ENROLLMENT_STATUSES.AWAITING_CHECKIN]: ENROLLMENT_STATUS_COLORS.AWAITING_CHECKIN,
+  [ENROLLMENT_STATUSES.AWAITING_ORIENTATION]: ENROLLMENT_STATUS_COLORS.AWAITING_ORIENTATION,
+  [ENROLLMENT_STATUSES.COMPLETED]: ENROLLMENT_STATUS_COLORS.COMPLETED,
+  [ENROLLMENT_STATUSES.JOB_SEARCH]: ENROLLMENT_STATUS_COLORS.JOB_SEARCH,
+  [ENROLLMENT_STATUSES.REMOVED_NONCOMPLIANT]: ENROLLMENT_STATUS_COLORS.REMOVED_NONCOMPLIANT,
+  [ENROLLMENT_STATUSES.SUCCESSFUL]: ENROLLMENT_STATUS_COLORS.SUCCESSFUL,
+  [ENROLLMENT_STATUSES.UNSUCCESSFUL]: ENROLLMENT_STATUS_COLORS.UNSUCCESSFUL,
+}, OL.GREY02);
+
+const shortHeaders = ['AGE', 'SENT. DATE', 'SENT. END DATE', '# OF WARN.', '# OF VIO.', 'HRS. SERVED'];
+const widthVariation = getStyleVariation('width', {
+  default: 'auto',
+  [shortHeaders[0]]: '55px',
+  [shortHeaders[1]]: '105px',
+  [shortHeaders[2]]: '105px',
+  [shortHeaders[3]]: '98px',
+  [shortHeaders[4]]: '83px',
+  [shortHeaders[5]]: '95px',
+}, 'auto');
+const whiteSpaceVariation = getStyleVariation('whiteSpace', {
+  default: 'normal',
+  [shortHeaders[0]]: 'nowrap',
+  [shortHeaders[1]]: '105px',
+  [shortHeaders[2]]: '105px',
+  [shortHeaders[3]]: 'nowrap',
+  [shortHeaders[4]]: 'nowrap',
+  [shortHeaders[5]]: 'nowrap',
+}, 'normal');
+
+const Cell = styled.td`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 12px;
+  padding: 7px 30px 7px 0;
+  text-align: left;
+  vertical-align: middle;
+  word-wrap: break-word;
+  color: ${statusColorVariation};
+  width: ${widthVariation};
+  white-space: ${whiteSpaceVariation};
+  ${(props) => props.cellStyle};
+`;
+
+const StyledParticipantsRow = styled.tr`
+  background-color: ${OL.WHITE};
+  border-bottom: 1px solid ${OL.GREY05};
+  font-size: 12px;
+  color: ${statusColorVariation};
+  padding: 7px 30px;
+
+  td,
+  th {
+    ${getStickyPosition}
+  }
+
+  ${Cell}:first-child {
+    padding-left: 30px;
+    width: 84px;
+    white-space: nowrap;
+  }
+
+  ${Cell}:last-child {
+    padding-right: 30px;
+  }
+
+  &:hover {
+    cursor: pointer;
+    background: ${OL.GREY14};
+  }
+
+  &:active {
+    background-color: ${OL.PURPLE06};
+  }
+`;
+
+export {
+  Cell,
+  StyledParticipantsRow,
+};
