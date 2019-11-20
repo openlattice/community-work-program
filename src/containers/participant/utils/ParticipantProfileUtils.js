cb2950fe53fb0d5e0c7ea7a3eb6166fda6a46521
Giulia <giuliaecampana@gmail.com>
2019-11-20 14:29:59 -0800

    display '----' if there are no dates associated with a CWP enrollment rather than displaying InvalidDateTime

--- src/containers/participant/utils/ParticipantProfileUtils.js
+++ src/containers/participant/utils/ParticipantProfileUtils.js
@@ -4,15 +4,30 @@ import { DateTime } from 'luxon';
 
 import { getEntityProperties } from '../../../utils/DataUtils';
 import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { EMPTY_FIELD } from '../../participants/ParticipantsConstants';
 
-const { DATETIME_RECEIVED } = PROPERTY_TYPE_FQNS;
+const { DATETIME_RECEIVED, CHECK_IN_DATETIME, ORIENTATION_DATETIME } = PROPERTY_TYPE_FQNS;
 
 const generateDiversionPlanOptions = (entities :List) :Object[] => {
   const options = [];
   entities.forEach((entity :Map) => {
-    const { [DATETIME_RECEIVED]: sentenceDateTime } = getEntityProperties(entity, [DATETIME_RECEIVED]);
-    const sentenceDate = DateTime.fromISO(sentenceDateTime).toLocaleString(DateTime.DATE_SHORT);
-    options.push({ label: `Enrollment ${sentenceDate}`, value: entity });
+    const {
+      [DATETIME_RECEIVED]: sentenceDateTime,
+      [CHECK_IN_DATETIME]: checkInDateTime,
+      [ORIENTATION_DATETIME]: orientationDateTime,
+    } = getEntityProperties(entity, [DATETIME_RECEIVED, CHECK_IN_DATETIME, ORIENTATION_DATETIME]);
+
+    let date :string = '';
+    const sentenceDateObj = DateTime.fromISO(sentenceDateTime);
+    const checkInDateObj = DateTime.fromISO(checkInDateTime);
+    const orientationDateObj = DateTime.fromISO(orientationDateTime);
+
+    if (sentenceDateObj.isValid) date = sentenceDateObj.toLocaleString(DateTime.DATE_SHORT);
+    else if (checkInDateObj.isValid) date = checkInDateObj.toLocaleString(DateTime.DATE_SHORT);
+    else if (orientationDateObj.isValid) date = orientationDateObj.toLocaleString(DateTime.DATE_SHORT);
+    else date = EMPTY_FIELD;
+
+    options.push({ label: `Enrollment ${date}`, value: entity });
   });
   return options;
 };
