3f3c5feba8d9363ade388a0f7c8460316e54077c
Giulia <giuliaecampana@gmail.com>
2019-12-03 18:55:01 -0800

    start integrating styled components from luk into AppContainer

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -6,10 +6,18 @@ import React, { Component } from 'react';
 
 import styled from 'styled-components';
 import { Map } from 'immutable';
-import { AuthActions } from 'lattice-auth';
+import {
+  AppContainerWrapper,
+  AppContentWrapper,
+  AppHeaderWrapper,
+  AppNavigationWrapper,
+  Sizes,
+} from 'lattice-ui-kit';
+import { AuthActions, AuthUtils } from 'lattice-auth';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import {
+  NavLink,
   Redirect,
   Route,
   Switch,
@@ -21,6 +29,7 @@ import AppHeaderContainer from './AppHeaderContainer';
 import DashboardContainer from '../dashboard/DashboardContainer';
 import EditWorksiteHoursForm from '../worksites/EditWorksiteHoursForm';
 import EditWorksiteInfoForm from '../worksites/EditWorksiteInfoForm';
+import OpenLatticeLogo from '../../assets/images/logo_v2.png';
 import ParticipantProfileContainer from '../participant/ParticipantProfileContainer';
 import ParticipantsSearchContainer from '../participants/ParticipantsSearchContainer';
 import PrintWorkScheduleContainer from '../workschedule/PrintWorkScheduleContainer';
@@ -36,33 +45,35 @@ import { ContactSupport } from '../../components/controls/index';
 import { APP_CONTAINER_WIDTH } from '../../core/style/Sizes';
 import { OL } from '../../core/style/Colors';
 import { APP, STATE } from '../../utils/constants/ReduxStateConsts';
+import { isNonEmptyString } from '../../utils/LangUtils';
 
+const { APP_CONTENT_WIDTH } = Sizes;
 const { logout } = AuthActions;
 
-const AppContainerWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  margin: 0;
-  min-width: ${APP_CONTAINER_WIDTH}px;
-  padding: 0;
-`;
+// const AppContainerWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   height: 100%;
+//   margin: 0;
+//   min-width: ${APP_CONTAINER_WIDTH}px;
+//   padding: 0;
+// `;
 
-const AppContentOuterWrapper = styled.main`
-  background-color: ${OL.GREY38};
-  display: flex;
-  flex: 1 0 auto;
-  justify-content: center;
-  position: relative;
-`;
-
-const AppContentInnerWrapper = styled.div`
-  display: flex;
-  flex: 1 0 auto;
-  flex-direction: column;
-  justify-content: flex-start;
-  position: relative;
-`;
+// const AppContentOuterWrapper = styled.main`
+//   background-color: ${OL.GREY38};
+//   display: flex;
+//   flex: 1 0 auto;
+//   justify-content: center;
+//   position: relative;
+// `;
+//
+// const AppContentInnerWrapper = styled.div`
+//   display: flex;
+//   flex: 1 0 auto;
+//   flex-direction: column;
+//   justify-content: flex-start;
+//   position: relative;
+// `;
 
 type Props = {
   actions:{
@@ -96,6 +107,11 @@ class AppContainer extends Component<Props> {
     }
   }
 
+  logout = () => {
+    const { actions } = this.props;
+    actions.logout();
+  }
+
   renderAppContent = () => (
     <Switch>
       <Route path={Routes.EDIT_WORKSITE_HOURS} component={EditWorksiteHoursForm} />
@@ -123,23 +139,41 @@ class AppContainer extends Component<Props> {
       return { label, value };
     });
     const loading = initializeAppRequestState === RequestStates.PENDING;
+
+    const userInfo :Object = AuthUtils.getUserInfo() || {};
+    let user = null;
+    if (isNonEmptyString(userInfo.name)) {
+      user = userInfo.name;
+    }
+    else if (isNonEmptyString(userInfo.email)) {
+      user = userInfo.email;
+    }
+
     return (
       <AppContainerWrapper>
         {
           !isPrintView && (
-            <AppHeaderContainer
-                loading={loading}
-                organizations={orgList}
-                selectedOrg={selectedOrg}
-                switchOrg={this.switchOrganization} />
+            <AppHeaderWrapper
+                appIcon={OpenLatticeLogo}
+                appTitle="Community Work Program"
+                logout={this.logout}
+                organizationsSelect={{
+                  onChange: this.switchOrganization,
+                  organizations: orgList,
+                }}
+                user={user}>
+              <AppNavigationWrapper>
+                <NavLink to={Routes.DASHBOARD}>Dashboard</NavLink>
+                <NavLink to={Routes.PARTICIPANTS}>Participants</NavLink>
+                <NavLink to={Routes.WORKSITES}>Work Sites</NavLink>
+                <NavLink to={Routes.WORK_SCHEDULE}>Work Schedule</NavLink>
+              </AppNavigationWrapper>
+            </AppHeaderWrapper>
           )
         }
-        <AppContentOuterWrapper>
-          <AppContentInnerWrapper>
-            { this.renderAppContent() }
-            <ContactSupport />
-          </AppContentInnerWrapper>
-        </AppContentOuterWrapper>
+        <AppContentWrapper contentWidth={APP_CONTENT_WIDTH}>
+          { this.renderAppContent() }
+        </AppContentWrapper>
       </AppContainerWrapper>
     );
   }
