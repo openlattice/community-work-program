74bcee1c53496629158dea0bf5b6733f1d789cb0
Giulia <giuliaecampana@gmail.com>
2019-12-05 11:48:38 -0800

    remove tertiary button option from toolbar

--- src/components/controls/ToolBar.js
+++ src/components/controls/ToolBar.js
@@ -2,32 +2,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import { List, Map } from 'immutable';
-import { Button, Select } from 'lattice-ui-kit';
+import { Button, Select, Sizes } from 'lattice-ui-kit';
 
 import SearchContainer from '../../containers/search/SearchContainer';
 import { ButtonWrapper, ButtonsWrapper } from '../Layout';
 
 import { OL } from '../../core/style/Colors';
-import { APP_CONTAINER_MAX_WIDTH, APP_CONTAINER_WIDTH, APP_CONTENT_PADDING } from '../../core/style/Sizes';
+import { APP_CONTAINER_MAX_WIDTH, APP_CONTAINER_WIDTH } from '../../core/style/Sizes';
+
+const { APP_CONTENT_WIDTH } = Sizes;
 
 const ToolBarWrapper = styled.div`
-  display: flex;
-  justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 60px;
-  border-bottom: 1px solid ${OL.GREY11};
   background-color: ${OL.WHITE};
+  border-bottom: 1px solid ${OL.GREY11};
+  display: flex;
+  height: 60px;
+  justify-content: center;
+  width: 100%;
 `;
 
 const ToolBarInnerWrapper = styled.div`
-  height: 100%;
-  max-width: ${APP_CONTAINER_MAX_WIDTH}px;
-  min-width: ${APP_CONTAINER_WIDTH}px;
+  align-self: center;
+  align-items: center;
   display: flex;
   flex: 1 0 auto;
+  height: 100%;
   justify-content: space-between;
-  align-items: center;
+  /* max-width: ${APP_CONTAINER_MAX_WIDTH}px;
+  min-width: ${APP_CONTAINER_WIDTH}px; */
+  width: ${APP_CONTENT_WIDTH}px;
   padding: 0 30px;
 `;
 
@@ -52,8 +56,6 @@ type Props = {
   primaryButtonAction :() => void;
   primaryButtonText :string;
   search :(input :string) => void;
-  tertiaryButtonAction :() => void;
-  tertiaryButtonText :string;
 };
 
 const ToolBar = ({
@@ -62,50 +64,35 @@ const ToolBar = ({
   primaryButtonAction,
   primaryButtonText,
   search,
-  tertiaryButtonAction,
-  tertiaryButtonText,
 } :Props) => (
-  <ToolBarWrapper>
-    <ToolBarInnerWrapper>
-      <ActionsWrapper>
-        <SearchContainer
-            search={search} />
-        {
-          dropdowns.map((dropdownMap :Map) => {
-            return (
-              <SelectWrapper key={`${dropdownMap.get('title')}-wrapper`}>
-                <Select
-                    key={dropdownMap.get('title')}
-                    onChange={onSelectFunctions.get(dropdownMap.get('title'))}
-                    options={dropdownMap.get('enums')}
-                    placeholder={dropdownMap.get('title')} />
-              </SelectWrapper>
-            );
-          })
-        }
-      </ActionsWrapper>
-      <ButtonsWrapper>
-        {
-          (tertiaryButtonAction && tertiaryButtonText)
-            ? (
-              <ButtonWrapper>
-                <Button onClick={tertiaryButtonAction}>{ tertiaryButtonText }</Button>
-              </ButtonWrapper>
-            )
-            : null
-        }
-        {
-          (primaryButtonAction && primaryButtonText)
-            ? (
-              <ButtonWrapper>
-                <Button mode="primary" onClick={primaryButtonAction}>{ primaryButtonText }</Button>
-              </ButtonWrapper>
-            )
-            : null
-        }
-      </ButtonsWrapper>
-    </ToolBarInnerWrapper>
-  </ToolBarWrapper>
+  <ToolBarInnerWrapper>
+    <ActionsWrapper>
+      <SearchContainer
+          search={search} />
+      {
+        dropdowns.map((dropdownMap :Map) => (
+          <SelectWrapper key={`${dropdownMap.get('title')}-wrapper`}>
+            <Select
+                key={dropdownMap.get('title')}
+                onChange={onSelectFunctions.get(dropdownMap.get('title'))}
+                options={dropdownMap.get('enums')}
+                placeholder={dropdownMap.get('title')} />
+          </SelectWrapper>
+        ))
+      }
+    </ActionsWrapper>
+    <ButtonsWrapper>
+      {
+        (primaryButtonAction && primaryButtonText)
+          ? (
+            <ButtonWrapper>
+              <Button mode="primary" onClick={primaryButtonAction}>{ primaryButtonText }</Button>
+            </ButtonWrapper>
+          )
+          : null
+      }
+    </ButtonsWrapper>
+  </ToolBarInnerWrapper>
 );
 
 export default ToolBar;
