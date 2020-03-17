c79d21e4827fba0aad074d55619f83ee29ea4aa9
Giulia Campana <giuliaecampana@gmail.com>
2020-03-16 19:04:56 -0700

    add card header to graph card

--- src/containers/stats/StatsContainer.js
+++ src/containers/stats/StatsContainer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Map } from 'immutable';
 import {
   Card,
+  CardHeader,
   CardSegment,
   Colors,
   Skeleton,
@@ -86,6 +87,12 @@ const Category = styled.div`
   font-weight: 600;
 `;
 
+const GraphHeader = styled(CardHeader)`
+  color: ${BLACK};
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 const StatsBoxSkeleton = () => (
   <>
     <Skeleton height={36} width="20%" />
@@ -94,7 +101,6 @@ const StatsBoxSkeleton = () => (
 );
 
 const toolTipStyle :Object = {
-  alignItems: 'center',
   borderRadius: '3px',
   color: WHITE,
   display: 'flex',
@@ -193,6 +199,7 @@ const StatsContainer = ({
           }
         </StatsWrapper>
         <Card>
+          <GraphHeader>Number of Enrollments, Active Participants by Court Type</GraphHeader>
           <CardSegment padding="30px" vertical>
             {
               dataIsLoading
