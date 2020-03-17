2159f180e1f7d37d0a4536b0d1159f83db52097a
Giulia Campana <giuliaecampana@gmail.com>
2020-03-16 18:51:22 -0700

    util for participants by court type data

--- src/containers/stats/utils/StatsUtils.js
+++ src/containers/stats/utils/StatsUtils.js
@@ -1,29 +1,9 @@
 // @flow
-import { List, Map } from 'immutable';
+import { Map } from 'immutable';
 
-import { getEntityKeyId, getEntityProperties } from '../../../utils/DataUtils';
-import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { ENROLLMENT_STATUSES } from '../../../core/edm/constants/DataModelConsts';
+import { isDefined } from '../../../utils/LangUtils';
 
-const { STATUS } = PROPERTY_TYPE_FQNS;
-
-const ACTIVE_STATUSES :string[] = [
-  ENROLLMENT_STATUSES.ACTIVE,
-  ENROLLMENT_STATUSES.ACTIVE_REOPENED,
-  ENROLLMENT_STATUSES.AWAITING_CHECKIN,
-  ENROLLMENT_STATUSES.AWAITING_ORIENTATION,
-  ENROLLMENT_STATUSES.JOB_SEARCH,
-];
-
-const getCurrentlyActiveParticipants = (enrollmentByParticipant :Map, participants :List) :List => (
-  participants.filter((participant :Map) => {
-    const participantEKID :UUID = getEntityKeyId(participant);
-    const enrollmentStatus :Map = enrollmentByParticipant.get(participantEKID, Map());
-    const { [STATUS]: status } = getEntityProperties(enrollmentStatus, [STATUS]);
-    return ACTIVE_STATUSES.includes(status);
-  }));
-
-const formatCourtTypeGraphData = (enrollmentsByCourtTypeGraphData :Map) :Object[] => {
+const formatEnrollmentsCourtTypeData = (enrollmentsByCourtTypeGraphData :Map) :Object[] => {
   const graphData :Object[] = [];
   enrollmentsByCourtTypeGraphData.forEach((numberOfEnrollments :number, courtType :string) => {
     graphData.push({ y: numberOfEnrollments, x: courtType });
@@ -31,7 +11,17 @@ const formatCourtTypeGraphData = (enrollmentsByCourtTypeGraphData :Map) :Object[
   return graphData;
 };
 
+const formatPeopleCourtTypeData = (peopleByCourtTypeGraphData :Map) :Object[] => {
+  const graphData :Object[] = [];
+  peopleByCourtTypeGraphData.forEach((numberOfActiveParticipants :number, courtType :string) => {
+    if (isDefined(numberOfActiveParticipants) && isDefined(courtType)) {
+      graphData.push({ y: numberOfActiveParticipants, x: courtType });
+    }
+  });
+  return graphData;
+};
+
 export {
-  formatCourtTypeGraphData,
-  getCurrentlyActiveParticipants,
+  formatEnrollmentsCourtTypeData,
+  formatPeopleCourtTypeData,
 };
