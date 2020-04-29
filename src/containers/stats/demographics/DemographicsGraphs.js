70b19b3c09b999e6b1b42342f16c9f81c54c8c18
Giulia Campana <giuliaecampana@gmail.com>
2020-04-29 12:22:30 -0700

    placeholders for demographics and work site graphs

---
+++ src/containers/stats/demographics/DemographicsGraphs.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 24px;
+  justify-content center;
+`;
+
+const DemographicsGraphs = () => (
+  <Wrapper>Coming Soon</Wrapper>
+);
+
+export default DemographicsGraphs;
---
+++ src/containers/stats/worksite/WorkSiteGraphs.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 24px;
+  justify-content center;
+`;
+
+const WorkSiteGraphs = () => (
+  <Wrapper>Coming Soon</Wrapper>
+);
+
+export default WorkSiteGraphs;
