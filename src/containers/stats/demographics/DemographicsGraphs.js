d1a9ffdb477ece08d1939c1810e93b3b1a775c15
Giulia Campana <giuliaecampana@gmail.com>
2020-05-14 19:31:33 -0700

    Flesh out demographics graphs component

--- src/containers/stats/demographics/DemographicsGraphs.js
+++ src/containers/stats/demographics/DemographicsGraphs.js
@@ -1,10 +1,42 @@
 // @flow
 import React from 'react';
+import { Map } from 'immutable';
+import {
+  Card,
+  CardSegment,
+  CardStack,
+} from 'lattice-ui-kit';
+import { connect } from 'react-redux';
 
 import RaceChart from './RaceChart';
+import { GraphHeader } from '../styled/GraphStyles';
+import { STATE, STATS } from '../../../utils/constants/ReduxStateConsts';
 
-const DemographicsGraphs = () => (
-  <RaceChart />
+const { ETHNICITY_DEMOGRAPHICS, RACE_DEMOGRAPHICS } = STATS;
+
+type Props = {
+  ethnicityDemographics :Map;
+  raceDemographics :Map;
+};
+
+const DemographicsGraphs = ({ ethnicityDemographics, raceDemographics } :Props) => (
+  <CardStack>
+    <Card>
+      <GraphHeader>Race and Ethnicity</GraphHeader>
+      <CardSegment vertical>
+        <RaceChart ethnicityDemographics={ethnicityDemographics} raceDemographics={raceDemographics} />
+      </CardSegment>
+    </Card>
+  </CardStack>
 );
 
-export default DemographicsGraphs;
+const mapStateToProps = (state :Map) => {
+  const stats = state.get(STATE.STATS);
+  return {
+    [ETHNICITY_DEMOGRAPHICS]: stats.get(ETHNICITY_DEMOGRAPHICS),
+    [RACE_DEMOGRAPHICS]: stats.get(RACE_DEMOGRAPHICS),
+  };
+};
+
+// $FlowFixMe
+export default connect(mapStateToProps, null)(DemographicsGraphs);
