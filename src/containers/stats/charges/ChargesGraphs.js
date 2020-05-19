7a8e65b580456e699737e91faedb3229a7a422a4
Giulia Campana <giuliaecampana@gmail.com>
2020-05-19 16:39:24 -0700

    add court charges stats table

--- src/containers/stats/charges/ChargesGraphs.js
+++ src/containers/stats/charges/ChargesGraphs.js
@@ -5,6 +5,7 @@ import { List, Map } from 'immutable';
 import { CardStack } from 'lattice-ui-kit';
 import { connect } from 'react-redux';
 
+import ChargesHeadCell from '../../../components/table/ChargesHeadCell';
 import ChargesTableRow from '../../../components/table/ChargesTableRow';
 import TableHeaderRow from '../../../components/table/TableHeaderRow';
 import {
@@ -16,12 +17,15 @@ import {
 } from '../../../components/table/styled/index';
 import { generateTableHeaders } from '../../../utils/FormattingUtils';
 import { STATE, STATS } from '../../../utils/constants/ReduxStateConsts';
-import { ARREST_CHARGE_HEADERS } from '../consts/StatsConsts';
+import { ARREST_CHARGE_HEADERS, COURT_CHARGE_HEADERS } from '../consts/StatsConsts';
 
-const { ARREST_CHARGE_TABLE_DATA } = STATS;
+const { ARREST_CHARGE_TABLE_DATA, COURT_CHARGE_TABLE_DATA } = STATS;
 const arrestChargeTableHeaders :Object[] = generateTableHeaders(ARREST_CHARGE_HEADERS);
+const courtChargeTableHeaders :Object[] = generateTableHeaders(COURT_CHARGE_HEADERS);
 
-const ChargesHeadCell = styled(TableCell)`
+const ChargesCell = styled(TableCell)`
+  font-size: 14px;
+
   :first-child {
     padding-left: 40px;
     width: 600px;
@@ -33,10 +37,6 @@ const ChargesHeadCell = styled(TableCell)`
   }
 `;
 
-const ChargesCell = styled(ChargesHeadCell)`
-  font-size: 14px;
-`;
-
 const tableComponents :Object = {
   Cell: ChargesCell,
   HeadCell: ChargesHeadCell,
@@ -46,31 +46,43 @@ const tableComponents :Object = {
 
 type Props = {
   arrestChargeTableData :List;
+  courtChargeTableData :List;
 };
 
-const ChargesGraphs = ({ arrestChargeTableData } :Props) => {
-  return (
-    <CardStack>
-      <TableCard>
-        <TableHeader padding="40px">
-          <TableName>
-            Arrest Charges
-          </TableName>
-        </TableHeader>
-        <CustomTable
-            components={tableComponents}
-            data={arrestChargeTableData.toJS()}
-            headers={arrestChargeTableHeaders}
-            isLoading={false} />
-      </TableCard>
-    </CardStack>
-  );
-};
+const ChargesGraphs = ({ arrestChargeTableData, courtChargeTableData } :Props) => (
+  <CardStack>
+    <TableCard>
+      <TableHeader padding="40px">
+        <TableName>
+          Arrest Charges
+        </TableName>
+      </TableHeader>
+      <CustomTable
+          components={tableComponents}
+          data={arrestChargeTableData.toJS()}
+          headers={arrestChargeTableHeaders}
+          isLoading={false} />
+    </TableCard>
+    <TableCard>
+      <TableHeader padding="40px">
+        <TableName>
+          Court Charges
+        </TableName>
+      </TableHeader>
+      <CustomTable
+          components={tableComponents}
+          data={courtChargeTableData.toJS()}
+          headers={courtChargeTableHeaders}
+          isLoading={false} />
+    </TableCard>
+  </CardStack>
+);
 
 const mapStateToProps = (state :Map) => {
   const stats :Map = state.get(STATE.STATS);
   return {
     [ARREST_CHARGE_TABLE_DATA]: stats.get(ARREST_CHARGE_TABLE_DATA),
+    [COURT_CHARGE_TABLE_DATA]: stats.get(COURT_CHARGE_TABLE_DATA),
   };
 };
 
