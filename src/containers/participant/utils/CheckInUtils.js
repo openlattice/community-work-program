134dedfc78a4e0e72ac5f176dc19f756071623d0
Giulia Campana <giuliaecampana@gmail.com>
2020-05-28 11:03:20 -0700

    util to get weeks to include and hours worked per week

--- src/containers/participant/utils/CheckInUtils.js
+++ src/containers/participant/utils/CheckInUtils.js
@@ -1,7 +1,12 @@
 // @flow
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
+import { List, Map, fromJS } from 'immutable';
 
 import { get24HourTimeFromString } from '../../../utils/ScheduleUtils';
+import { getEntityProperties, sortEntitiesByDateProperty } from '../../../utils/DataUtils';
+import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+
+const { DATETIME_START, HOURS_WORKED } = PROPERTY_TYPE_FQNS;
 
 const get24HourTimeForCheckIn = (hours :string) :Object => {
   const hoursArray :string[] = hours.split('-');
@@ -22,7 +27,61 @@ const getHoursScheduled = (timeIn :string, timeOut :string) :number => {
   return 0;
 };
 
+const getWeeklyBreakdownOfHoursPerWeek = (checkIns :List) => {
+  if (checkIns.isEmpty()) return List();
+  const sortedCheckIns :List = sortEntitiesByDateProperty(checkIns, [DATETIME_START]);
+  const { [DATETIME_START]: firstDateTime } = getEntityProperties(sortedCheckIns.first(), [DATETIME_START]);
+  const { [DATETIME_START]: lastDateTime } = getEntityProperties(sortedCheckIns.last(), [DATETIME_START]);
+
+  const hoursByWeek :List = List().withMutations((list :List) => {
+
+    const firstWeekStartDate :string = DateTime.fromISO(firstDateTime).startOf('week').toUTC().toISO();
+    const lastWeekStartDate :string = DateTime.fromISO(lastDateTime).startOf('week').toUTC().toISO();
+
+    const weeksDuration = DateTime.fromISO(lastWeekStartDate).diff(DateTime.fromISO(firstWeekStartDate), 'weeks');
+    // $FlowFixMe
+    const { weeks } = weeksDuration.toObject();
+
+    let counter = 0;
+    while (counter < weeks + 1) {
+      let weekStart;
+      let weekEnd;
+      if (list.isEmpty()) {
+        weekStart = firstWeekStartDate;
+        weekEnd = DateTime.fromISO(firstDateTime).endOf('week').toUTC().toISO();
+      }
+      else {
+        const weekLongDuration = Duration.fromObject({ weeks: 1 });
+        const previousWeek = list.last();
+        const previousWeekStartDate = previousWeek.get('weekStart');
+        weekStart = DateTime.fromISO(previousWeekStartDate).plus(weekLongDuration).toISO();
+        weekEnd = DateTime.fromISO(weekStart).endOf('week').toUTC().toISO();
+      }
+      list.push(fromJS({ weekStart, weekEnd, hours: 0 }));
+      counter += 1;
+    }
+
+    sortedCheckIns.forEach((checkIn :Map) => {
+      const {
+        [DATETIME_START]: checkInStart,
+        [HOURS_WORKED]: checkInHours,
+      } = getEntityProperties(checkIn, [DATETIME_START, HOURS_WORKED]);
+      const startDateTime :DateTime = DateTime.fromISO(checkInStart);
+      list.forEach((weekMap :Map, index :number) => {
+        const weekStart :DateTime = DateTime.fromISO(weekMap.get('weekStart'));
+        const weekEnd :DateTime = DateTime.fromISO(weekMap.get('weekEnd'));
+        // $FlowFixMe
+        if (startDateTime > weekStart && startDateTime < weekEnd) {
+          list.setIn([index, 'hours'], list.getIn([index, 'hours']) + checkInHours);
+        }
+      });
+    });
+  });
+  return hoursByWeek;
+};
+
 export {
   get24HourTimeForCheckIn,
   getHoursScheduled,
+  getWeeklyBreakdownOfHoursPerWeek,
 };
