2094e29aea5073ee038b01b64b95fc92ab932d91
Giulia Campana <giuliaecampana@gmail.com>
2020-06-02 23:57:34 -0700

    util for formatting data for search results

---
+++ src/containers/participants/utils/NewParticipantUtils.js
@@ -0,0 +1,26 @@
+// @flow
+import { List, Map } from 'immutable';
+
+import { getPersonDOB, getPersonFullName } from '../../../utils/PeopleUtils';
+
+const formatExistingPeopleData = (peopleAlreadyInEntitySet :List) => {
+
+  const existingPeopleData :List = List().withMutations((list :List) => {
+    peopleAlreadyInEntitySet.forEach((person :Map) => {
+      const personMap :Map = Map().withMutations((map :Map) => {
+        const personName :string = getPersonFullName(person);
+        const personDOB :string = getPersonDOB(person);
+        map.set('name', personName);
+        map.set('dob', personDOB);
+      });
+      list.push(personMap);
+    });
+  });
+
+  return existingPeopleData;
+};
+
+/* eslint-disable import/prefer-default-export */
+export {
+  formatExistingPeopleData,
+};
