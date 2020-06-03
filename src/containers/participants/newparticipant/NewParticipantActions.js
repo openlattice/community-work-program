a9acb64eaac62a365ec25bc92a8acc8c2b05e764
Giulia Campana <giuliaecampana@gmail.com>
2020-06-03 09:46:00 -0700

    select existing person action from search results

--- src/containers/participants/newparticipant/NewParticipantActions.js
+++ src/containers/participants/newparticipant/NewParticipantActions.js
@@ -5,7 +5,12 @@ import type { RequestSequence } from 'redux-reqseq';
 const SEARCH_EXISTING_PEOPLE :'SEARCH_EXISTING_PEOPLE' = 'SEARCH_EXISTING_PEOPLE';
 const searchExistingPeople :RequestSequence = newRequestSequence(SEARCH_EXISTING_PEOPLE);
 
+const SELECT_EXISTING_PERSON :'SELECT_EXISTING_PERSON' = 'SELECT_EXISTING_PERSON';
+const selectExistingPerson :RequestSequence = newRequestSequence(SELECT_EXISTING_PERSON);
+
 export {
   SEARCH_EXISTING_PEOPLE,
+  SELECT_EXISTING_PERSON,
   searchExistingPeople,
+  selectExistingPerson,
 };
