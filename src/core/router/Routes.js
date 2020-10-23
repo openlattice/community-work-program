2f5a4e98cc1ea20fb38104a0111c183c0f964d1d
Giulia Campana <giuliaecampana@gmail.com>
2020-10-22 17:14:26 -0700

    Add PARTICIPANT_ID const to Routes

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -12,7 +12,8 @@ export const STATS :string = '/stats';
 
 export const ADD_PARTICIPANT :string = `${PARTICIPANTS}/add`;
 
-export const PARTICIPANT_PROFILE :string = `${PARTICIPANTS}/:participantId`;
+export const PARTICIPANT_ID :string = 'participantId';
+export const PARTICIPANT_PROFILE :string = `${PARTICIPANTS}/:${PARTICIPANT_ID}`;
 export const PRINT_PARTICIPANT_SCHEDULE :string = `${PARTICIPANT_PROFILE}/schedule/print`;
 const EDIT_PROFILE :string = `${PARTICIPANT_PROFILE}/edit`;
 export const EDIT_PARTICIPANT :string = `${EDIT_PROFILE}/participant`;
