94c42dbd63ec9d0bb42dc048a1a6756c5e863958
Giulia Campana <32921059+giuliacampana@users.noreply.github.com>
2021-06-22 14:23:36 -0700

    add ProgramOutcome to ReduxReducer

--- src/core/redux/ReduxReducer.js
+++ src/core/redux/ReduxReducer.js
@@ -14,6 +14,7 @@ import participantReducer from '../../containers/participant/ParticipantReducer'
 import participantsReducer from '../../containers/participants/ParticipantsReducer';
 import personContactsReducer from '../../containers/participant/contacts/PersonContactsReducer';
 import printParticipantReducer from '../../containers/participant/print/PrintParticipantReducer';
+import programOutcomeReducer from '../../containers/participant/outcome/ProgramOutcomeReducer';
 import searchReducer from '../../containers/search/reducers';
 import statsReducer from '../../containers/stats/StatsReducer';
 import workScheduleReducer from '../../containers/workschedule/WorkScheduleReducer';
@@ -32,6 +33,7 @@ export default function reduxReducer(routerHistory :any) {
     person: participantReducer,
     personContacts: personContactsReducer,
     printParticipant: printParticipantReducer,
+    programOutcome: programOutcomeReducer,
     router: connectRouter(routerHistory),
     search: searchReducer,
     stats: statsReducer,
